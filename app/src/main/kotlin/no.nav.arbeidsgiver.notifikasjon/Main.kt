abd0edd93258ca2f2d8ff9b1a43e16cc4253cb22
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-03-15 13:20:40 +0100

    enable prometheus metrics

--- app/src/main/kotlin/no.nav.arbeidsgiver.notifikasjon/Main.kt
+++ app/src/main/kotlin/no.nav.arbeidsgiver.notifikasjon/Main.kt
@@ -2,13 +2,22 @@ package no.nav.arbeidsgiver.notifikasjon
 
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.metrics.micrometer.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
 
 fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
+    val registry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
     embeddedServer(Netty, port = 8080) {
+        install(MicrometerMetrics) {
+            this.registry = registry
+        }
+
         routing {
             route("internal") {
                 get ("alive") {
@@ -16,6 +25,10 @@ fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
                 }
                 get ("ready") {
                     call.respond(HttpStatusCode.OK)
+                }
+
+                get("metrics") {
+
                 }
             }
         }
