abbcfaba1edd68b79315e772ca9a541cd341f050
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-03-15 11:08:40 +0100

    Logstash logging and fnr mask

--- app/src/main/kotlin/no.nav.arbeidsgiver.notifikasjon/Main.kt
+++ app/src/main/kotlin/no.nav.arbeidsgiver.notifikasjon/Main.kt
@@ -8,8 +8,6 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
-    println("hello world")
-
     embeddedServer(Netty, port = 8080) {
         routing {
             route("internal") {
