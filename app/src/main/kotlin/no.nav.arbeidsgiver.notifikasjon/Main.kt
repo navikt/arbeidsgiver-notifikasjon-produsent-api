3f87a975e92cf885868e17ac7a26f9c61b1b242c
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-03-15 14:02:34 +0100

    add call logging, correlation id, exception handler

--- app/src/main/kotlin/no.nav.arbeidsgiver.notifikasjon/Main.kt
+++ app/src/main/kotlin/no.nav.arbeidsgiver.notifikasjon/Main.kt
@@ -1,14 +1,19 @@
 package no.nav.arbeidsgiver.notifikasjon
 
+import com.fasterxml.jackson.core.JsonProcessingException
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.metrics.micrometer.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
+import org.slf4j.event.Level
+import java.util.*
 
 fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
     val meterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
@@ -16,6 +21,51 @@ fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
     embeddedServer(Netty, port = 8080) {
         install(MicrometerMetrics) {
             registry = meterRegistry
+        }
+
+        install(CallId) {
+            retrieveFromHeader(HttpHeaders.XRequestId)
+            retrieveFromHeader(HttpHeaders.XCorrelationId)
+            retrieveFromHeader("call-id")
+            retrieveFromHeader("callId")
+            retrieveFromHeader("call_id")
+
+            generate {
+                UUID.randomUUID().toString()
+            }
+
+            replyToHeader(HttpHeaders.XCorrelationId)
+        }
+
+        install(CallLogging) {
+            filter { call ->
+                !call.request.path().startsWith("/internal/")
+            }
+
+            level = Level.INFO
+            mdc("method") { call ->
+                call.request.httpMethod.value
+            }
+            mdc("path") { call ->
+                call.request.path()
+            }
+            callIdMdc("x_correlation_id")
+        }
+
+        install(StatusPages) {
+            exception<Throwable> { ex ->
+                if (ex is JsonProcessingException) {
+                    // ikke logg json-teksten som feilet.
+                    ex.clearLocation()
+                }
+
+                log.warn("unhandle exception in ktor pipeline: {}", ex::class.qualifiedName, ex)
+                call.respond(
+                    HttpStatusCode.InternalServerError, mapOf(
+                        "error" to "unexpected error",
+                    )
+                )
+            }
         }
 
         routing {
