e958f380a07425ce5dbe8ceb614cb24e4a304505
Ken Gullaksen <ken.gullaksen@nav.no>
2021-04-07 11:21:08 +0200

    opprett kafka.properties i init script

---
+++ app/99-create-kafka-properties.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+echo "
+security.protocol=SSL
+ssl.keystore.location=$KAFKA_KEYSTORE_PATH
+ssl.keystore.password=$KAFKA_CREDSTORE_PASSWORD
+ssl.truststore.location=$KAFKA_TRUSTSTORE_PATH
+ssl.truststore.password=$KAFKA_CREDSTORE_PASSWORD
+bootstrap.servers=$KAFKA_BROKERS
+" > /kafka.properties
