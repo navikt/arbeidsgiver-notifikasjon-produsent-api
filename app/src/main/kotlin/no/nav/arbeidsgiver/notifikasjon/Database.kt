4febf35c00201c00144487c017e6e185d2a27625
Ken Gullaksen <ken.gullaksen@nav.no>
2021-04-12 10:43:32 +0200

    reload fra env hver poll

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/Database.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/Database.kt
@@ -7,18 +7,23 @@ import org.flywaydb.core.api.output.MigrateResult
 import java.sql.Connection
 import javax.sql.DataSource
 
-val hikariConfig = HikariConfig().apply {
-    val host = System.getenv("DB_HOST") ?: "localhost"
-    val port = System.getenv("DB_PORT") ?: "5432"
-    val db = System.getenv("DB_DATABASE") ?: "postgres"
+fun hikariConfig() : HikariConfig {
+    return HikariConfig().apply {
+        val host = System.getenv("DB_HOST") ?: "localhost"
+        val port = System.getenv("DB_PORT") ?: "5432"
+        val db = System.getenv("DB_DATABASE") ?: "postgres"
 
-    username =  System.getenv("DB_USERNAME") ?: "postgres"
-    password = System.getenv("DB_PASSWORD") ?: "postgres"
-    jdbcUrl = "jdbc:postgresql://$host:$port/$db"
-    driverClassName = "org.postgresql.Driver"
+        jdbcUrl = "jdbc:postgresql://$host:$port/$db"
+        username = System.getenv("DB_USERNAME") ?: "postgres"
+        password = System.getenv("DB_PASSWORD") ?: "postgres"
+        driverClassName = "org.postgresql.Driver"
+    }
 }
 
-fun hikariDatasource(config: HikariConfig = hikariConfig) = HikariDataSource(config)
+fun hikariDatasource(): HikariDataSource {
+    val hikariConfig = hikariConfig()
+    return HikariDataSource(hikariConfig)
+}
 
 object DB {
     val dataSource: DataSource
