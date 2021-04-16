7f659a0f8f006575967aca48c685096c1209a232
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-04-16 12:29:07 +0200

    Log json i cluster

---
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/LogConfig.kt
@@ -0,0 +1,44 @@
+package no.nav.arbeidsgiver.notifikasjon
+
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.classic.layout.TTLLLayout
+import ch.qos.logback.classic.spi.Configurator
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.encoder.LayoutWrappingEncoder
+import ch.qos.logback.core.spi.ContextAware
+import ch.qos.logback.core.spi.ContextAwareBase
+import ch.qos.logback.core.spi.LifeCycle
+import net.logstash.logback.encoder.LogstashEncoder
+import no.nav.common.log.MaskingAppender
+
+class LogConfig : ContextAwareBase(), Configurator {
+    override fun configure(lc: LoggerContext) {
+        val rootAppender = MaskingAppender().setup(lc) {
+            setAppender(
+                ConsoleAppender<ILoggingEvent>().setup(lc) {
+                    if (System.getenv("NAIS_CLUSTER_NAME") != null) {
+                        encoder = LogstashEncoder().setup(lc)
+                    } else {
+                        encoder = LayoutWrappingEncoder<ILoggingEvent>().setup(lc).apply {
+                            layout = TTLLLayout().setup(lc)
+                        }
+                    }
+                }
+            )
+        }
+
+        lc.getLogger(Logger.ROOT_LOGGER_NAME).addAppender(rootAppender)
+    }
+}
+
+private fun <T> T.setup(context: LoggerContext, body: T.() -> Unit = {}): T
+        where T : ContextAware,
+              T : LifeCycle
+{
+    this.context = context
+    this.body()
+    this.start()
+    return this
+}
