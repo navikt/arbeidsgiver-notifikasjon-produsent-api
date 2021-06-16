ea46612ae33c7fc76881d0a0b955e7936621f1c5
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-06-16 14:17:18 +0200

    fix main: static

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/Main.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/Main.kt
@@ -5,14 +5,13 @@ import no.nav.arbeidsgiver.notifikasjon.infrastruktur.*
 @Suppress("unused") /* Entry point for jar. */
 object Main {
     val log = logger()
-
-    fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
-        when (val navn = System.getenv("NAIS_APP_NAME")) {
-            "notifikasjon-produsent-api" -> ProdusentMain.main()
-            "notifikasjon-bruker-api" -> BrukerMain.main()
-            else -> log.error("ukjent \$NAIS_APP_NAME '$navn'")
-        }
-    }
 }
 
+fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
+    when (val navn = System.getenv("NAIS_APP_NAME")) {
+        "notifikasjon-produsent-api" -> ProdusentMain.main()
+        "notifikasjon-bruker-api" -> BrukerMain.main()
+        else -> Main.log.error("ukjent \$NAIS_APP_NAME '$navn'")
+    }
+}
 
