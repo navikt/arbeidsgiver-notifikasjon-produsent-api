09bba63f9c4e36770fbdf52070cd6011da72c0ed
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-09-20 13:34:36 +0200

    Refactor mottakerregister slik at det beregnes ut fra produsentregister (#35)
    * Refactor mottakerregister slik at det beregnes ut fra produsentregister
    
    * fjern imports som blokkerer deploy
    
    * lazy load globale variabler

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/infrastruktur/produsenter/ProdusentRegisterImpl.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/infrastruktur/produsenter/ProdusentRegisterImpl.kt
@@ -2,23 +2,6 @@ package no.nav.arbeidsgiver.notifikasjon.infrastruktur.produsenter
 
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.basedOnEnv
 
-val MOTTAKER_REGISTER: List<MottakerDefinisjon> = listOf(
-//    ServicecodeDefinisjon(code = "5216", version = "1", description = "Mentortilskudd"),
-//    ServicecodeDefinisjon(code = "5212", version = "1", description = "Inkluderingstilskudd"),
-//    ServicecodeDefinisjon(code = "5384", version = "1", description = "Ekspertbistand"),
-//    ServicecodeDefinisjon(code = "5159", version = "1", description = "Lønnstilskudd"),
-    ServicecodeDefinisjon(code = "4936", version = "1", description = "Inntektsmelding"),
-//    ServicecodeDefinisjon(code = "5332", version = "2", description = "Arbeidstrening"),
-    ServicecodeDefinisjon(code = "5332", version = "1", description = "Arbeidstrening"),
-//    ServicecodeDefinisjon(code = "5441", version = "1", description = "Arbeidsforhold"),
-    ServicecodeDefinisjon(code = "5516", version = "1", description = "Midlertidig lønnstilskudd"),
-    ServicecodeDefinisjon(code = "5516", version = "2", description = "Varig lønnstilskudd'"),
-    ServicecodeDefinisjon(code = "5516", version = "3", description = "Sommerjobb'"),
-//    ServicecodeDefinisjon(code = "3403", version = "2", description = "Sykfraværsstatistikk"),
-//    ServicecodeDefinisjon(code = "5078", version = "1", description = "Rekruttering"),
-//    ServicecodeDefinisjon(code = "5278", version = "1", description = "Tilskuddsbrev om NAV-tiltak"),
-)
-
 val FAGER_TESTPRODUSENT =
     Produsent(
         accessPolicy = basedOnEnv(
@@ -43,22 +26,36 @@ val ARBEIDSGIVER_TILTAK = Produsent(
     ),
     tillatteMerkelapper = listOf("Tiltak"),
     tillatteMottakere = listOf(
-        ServicecodeDefinisjon(code = "4936", version = "1"),
-        ServicecodeDefinisjon(code = "5332", version = "1"),
-        ServicecodeDefinisjon(code = "5516", version = "1"),
-        ServicecodeDefinisjon(code = "5516", version = "2"),
-        ServicecodeDefinisjon(code = "5516", version = "3"),
+        ServicecodeDefinisjon(code = "4936", version = "1", description = "Inntektsmelding"),
+        ServicecodeDefinisjon(code = "5332", version = basedOnEnv(prod = "2", other = "1"), description = "Arbeidstrening"),
+        ServicecodeDefinisjon(code = "5516", version = "1", description = "Midlertidig lønnstilskudd"),
+        ServicecodeDefinisjon(code = "5516", version = "2", description = "Varig lønnstilskudd'"),
+        ServicecodeDefinisjon(code = "5516", version = "3", description = "Sommerjobb'"),
     )
 )
 
-
-val PRODUSENT_REGISTER = ProdusentRegisterImpl(
-    basedOnEnv(
-        prod = listOf(),
-        other = listOf(
-            FAGER_TESTPRODUSENT,
-            ARBEIDSGIVER_TILTAK
-        )
+val PRODUSENT_LIST = basedOnEnv(
+    prod = listOf(),
+    other = listOf(
+        FAGER_TESTPRODUSENT,
+        ARBEIDSGIVER_TILTAK
     )
 )
 
+val PRODUSENT_REGISTER by lazy { ProdusentRegisterImpl(PRODUSENT_LIST) }
+
+val MOTTAKER_REGISTER: List<MottakerDefinisjon> by lazy {
+    PRODUSENT_LIST
+        .flatMap { it.tillatteMottakere }
+        .distinct()
+}
+
+//    ServicecodeDefinisjon(code = "5216", version = "1", description = "Mentortilskudd"),
+//    ServicecodeDefinisjon(code = "5212", version = "1", description = "Inkluderingstilskudd"),
+//    ServicecodeDefinisjon(code = "5384", version = "1", description = "Ekspertbistand"),
+//    ServicecodeDefinisjon(code = "5159", version = "1", description = "Lønnstilskudd"),
+//    ServicecodeDefinisjon(code = "5332", version = "2", description = "Arbeidstrening"),
+//    ServicecodeDefinisjon(code = "5441", version = "1", description = "Arbeidsforhold"),
+//    ServicecodeDefinisjon(code = "3403", version = "2", description = "Sykfraværsstatistikk"),
+//    ServicecodeDefinisjon(code = "5078", version = "1", description = "Rekruttering"),
+//    ServicecodeDefinisjon(code = "5278", version = "1", description = "Tilskuddsbrev om NAV-tiltak"),
