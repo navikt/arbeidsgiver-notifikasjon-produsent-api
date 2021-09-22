4f0723f0f0da527464712a9109d1dd46281da11a
Ken Gullaksen <ken.gullaksen@nav.no>
2021-09-22 13:21:07 +0200

    bytt til mer stabil download url

--- download_kafka_cli.sh
+++ download_kafka_cli.sh
@@ -3,7 +3,7 @@
 rm -rf .kafka-cli
 mkdir -p .kafka-cli
 pushd .kafka-cli
-curl https://downloads.apache.org/kafka/2.7.1/kafka_2.13-2.7.1.tgz --output kafka.tgz
+curl https://archive.apache.org/dist/kafka/2.7.1/kafka_2.13-2.7.1.tgz --output kafka.tgz
 tar --strip-components=1 -xzf kafka.tgz
 chmod +x bin/*
 echo "
