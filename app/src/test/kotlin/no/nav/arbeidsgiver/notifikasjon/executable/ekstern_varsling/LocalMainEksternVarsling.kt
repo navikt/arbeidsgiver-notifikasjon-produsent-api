0de9492593ea437cf3b8b587672d4dd6e4243796
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-11-04 15:53:03 +0100

    Expose metric for emergency break (#74)

--- app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/executable/ekstern_varsling/LocalMainEksternVarsling.kt
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/executable/ekstern_varsling/LocalMainEksternVarsling.kt
@@ -1,24 +1,12 @@
 package no.nav.arbeidsgiver.notifikasjon.executable.ekstern_varsling
 
-import com.fasterxml.jackson.databind.node.NullNode
 import db.migration.OS
 import no.nav.arbeidsgiver.notifikasjon.EksternVarsling
-import no.nav.arbeidsgiver.notifikasjon.ekstern_varsling.AltinnVarselKlient
-import no.nav.arbeidsgiver.notifikasjon.ekstern_varsling.EksternVarsel
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.logger
-
 
 fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
     OS.setupLocal()
     EksternVarsling.main(
         httpPort = 8085,
-        altinnVarselKlient = object: AltinnVarselKlient {
-            val log = logger()
-            override suspend fun send(eksternVarsel: EksternVarsel): Result<AltinnVarselKlient.AltinnResponse> {
-                log.info("stub altinn varsel klient, send: $eksternVarsel")
-                return Result.success(AltinnVarselKlient.AltinnResponse.Ok(NullNode.instance))
-            }
-        }
     )
 }
 
