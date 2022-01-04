96fc5259790c85ab5fd802576f073826a4a0df85
Ken Gullaksen <ken.gullaksen@nav.no>
2022-01-04 13:02:01 +0100

    test for idempotent oppførsel i resterende modeller

---
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/produsent_api/ProdusentModelIdempotensTests.kt
@@ -0,0 +1,23 @@
+package no.nav.arbeidsgiver.notifikasjon.produsent_api
+
+import io.kotest.core.datatest.forAll
+import io.kotest.core.spec.style.DescribeSpec
+import no.nav.arbeidsgiver.notifikasjon.Hendelse
+import no.nav.arbeidsgiver.notifikasjon.Produsent
+import no.nav.arbeidsgiver.notifikasjon.produsent.ProdusentRepositoryImpl
+import no.nav.arbeidsgiver.notifikasjon.util.EksempelHendelse
+import no.nav.arbeidsgiver.notifikasjon.util.testDatabase
+
+class ProdusentModelIdempotensTests : DescribeSpec({
+    val database = testDatabase(Produsent.databaseConfig)
+    val produsentModel = ProdusentRepositoryImpl(database)
+
+    describe("Idempotent oppførsel") {
+        forAll<Hendelse>(EksempelHendelse.Alle) { hendelse ->
+            it("håndterer ${hendelse::class.simpleName} med idempotens") {
+                produsentModel.oppdaterModellEtterHendelse(hendelse)
+                produsentModel.oppdaterModellEtterHendelse(hendelse)
+            }
+        }
+    }
+})
