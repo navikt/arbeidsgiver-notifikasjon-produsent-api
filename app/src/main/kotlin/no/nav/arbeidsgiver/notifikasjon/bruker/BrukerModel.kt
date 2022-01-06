39d0f74da1363662cedf2287114d75e4b69755b2
bendiksi <bendik.segrov.ibenholt@nav.no>
2022-01-06 11:07:43 +0100

    legg til counter for antall notifikasjoner som er hentet

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/BrukerModel.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/BrukerModel.kt
@@ -103,6 +103,7 @@ class BrukerModelImpl(
 
 
     private val timer = Health.meterRegistry.timer("query_model_repository_hent_notifikasjoner")
+    private val notifikasjonerHentetCount = Health.meterRegistry.counter("notifikasjoner_hentet")
 
     override suspend fun hentNotifikasjoner(
         fnr: String,
@@ -144,6 +145,7 @@ class BrukerModelImpl(
                 jsonb(tilgangerAltinnMottaker)
             }
         ) {
+            notifikasjonerHentetCount.increment()
             when (val type = getString("type")) {
                 "BESKJED" -> BrukerModel.Beskjed(
                     merkelapp = getString("merkelapp"),
