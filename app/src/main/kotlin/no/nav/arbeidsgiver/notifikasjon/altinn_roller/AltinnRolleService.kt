fc57b44a0da9d3177af2646cfc2b7babf86d792b
Ken Gullaksen <ken.gullaksen@nav.no>
2022-02-21 16:23:41 +0100

    TAG-1837 marker som volatile

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/altinn_roller/AltinnRolleService.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/altinn_roller/AltinnRolleService.kt
@@ -14,6 +14,7 @@ class AltinnRolleServiceImpl(
     private val altinnRolleRepository: AltinnRolleRepository,
 ) : AltinnRolleService {
 
+    @Volatile
     private var alleRollerByCode: Map<String, AltinnRolle>? = null
 
     override suspend fun lastRollerFraAltinn() {
