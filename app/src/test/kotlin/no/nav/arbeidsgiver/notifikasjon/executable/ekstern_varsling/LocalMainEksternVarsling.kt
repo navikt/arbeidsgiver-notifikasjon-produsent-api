a86a2960a107ca9014b6000daa6bdea446052b5e
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-04-05 16:00:41 +0200

    fjern migration ops

--- app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/executable/ekstern_varsling/LocalMainEksternVarsling.kt
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/executable/ekstern_varsling/LocalMainEksternVarsling.kt
@@ -1,10 +1,8 @@
 package no.nav.arbeidsgiver.notifikasjon.executable.ekstern_varsling
 
-import db.migration.OS
 import no.nav.arbeidsgiver.notifikasjon.EksternVarsling
 
 fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
-    OS.setupLocal()
     EksternVarsling.main(
         httpPort = 8085,
     )
--- app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/executable/kafka_reaper/LocalMainKafkaReaper.kt
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/executable/kafka_reaper/LocalMainKafkaReaper.kt
@@ -1,11 +1,9 @@
 package no.nav.arbeidsgiver.notifikasjon.executable.kafka_reaper
 
-import db.migration.OS
 import no.nav.arbeidsgiver.notifikasjon.KafkaReaper
 
 /* Bruker API */
 fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
-    OS.setupLocal()
     KafkaReaper.main(
         httpPort = 8083
     )
