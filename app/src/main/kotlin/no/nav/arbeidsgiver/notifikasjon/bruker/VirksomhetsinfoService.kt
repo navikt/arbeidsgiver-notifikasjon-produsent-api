cf9f9af7d8f1cde39741b9f99ac2c7173dfd7df0
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-04-07 11:02:12 +0200

    debug logging

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/VirksomhetsinfoService.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/VirksomhetsinfoService.kt
@@ -4,18 +4,25 @@ import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.model.AltinnReportee
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Enhetsregisteret
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Enhetsregisteret.Underenhet
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.FunkyCache
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.logger
 
 class VirksomhetsinfoService(
     enhetsregisteret: Enhetsregisteret,
 ) {
-    private val cache = FunkyCache<String, Underenhet>(100_000) { orgnr ->
+    private val log = logger()
+
+    private val cache = FunkyCache<String, Underenhet>(600_000) { orgnr ->
+        log.debug("oppslag ereg $orgnr")
         enhetsregisteret.hentUnderenhet(orgnr)
     }
 
     suspend fun hentUnderenhet(virksomhetsnummer: String): Underenhet =
-        cache.get(virksomhetsnummer)
+        cache.get(virksomhetsnummer).also {
+            log.debug("hentUnderenhet($virksomhetsnummer) -> $it")
+        }
 
     fun altinnObserver(altinnReportee: AltinnReportee) {
+        log.debug("observe altinn $altinnReportee")
         val virksomhetsnummer = altinnReportee.organizationNumber ?: return
         cache.put(virksomhetsnummer, Underenhet(
             navn = altinnReportee.name,
