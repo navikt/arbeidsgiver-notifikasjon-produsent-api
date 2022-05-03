0ae305c8a8d00c964e9a15873d93df7a22eab6df
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-05-03 16:14:32 +0200

    ScheduleTime::parse implementert

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/autoslett/ScheduledTime.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/autoslett/ScheduledTime.kt
@@ -9,9 +9,15 @@ class ScheduledTime(
     private val spec: LocalDateTimeOrDuration,
     private val baseTime: OffsetDateTime
 ) {
-
     fun happensAt(): Instant = when (spec) {
         is LocalDateTimeOrDuration.LocalDateTime -> spec.value.atOslo().toInstant()
         is LocalDateTimeOrDuration.Duration -> (baseTime + spec.value).toInstant()
+    }
+
+    companion object {
+        fun parse(spec: String, base: String) = ScheduledTime(
+            spec = LocalDateTimeOrDuration.parse(spec),
+            baseTime = OffsetDateTime.parse(base),
+        )
     }
 }
