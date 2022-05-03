014f2f9268a7222b32d6eea77faceae30cc3cc7e
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-05-03 17:03:59 +0200

    Automatisk sletting modell: flere tester

--- app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/autoslett/AutoSlettRepositoryTests.kt
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/autoslett/AutoSlettRepositoryTests.kt
@@ -7,39 +7,83 @@ import io.kotest.matchers.shouldNotBe
 import no.nav.arbeidsgiver.notifikasjon.AutoSlett
 import no.nav.arbeidsgiver.notifikasjon.HendelseModel
 import no.nav.arbeidsgiver.notifikasjon.HendelseModel.LocalDateTimeOrDuration
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.ISO8601Period
+import no.nav.arbeidsgiver.notifikasjon.HendelseModel.NyTidStrategi.FORLENG
+import no.nav.arbeidsgiver.notifikasjon.HendelseModel.NyTidStrategi.OVERSKRIV
 import no.nav.arbeidsgiver.notifikasjon.produsent.api.IdempotenceKey
 import no.nav.arbeidsgiver.notifikasjon.tid.atOslo
 import no.nav.arbeidsgiver.notifikasjon.util.testDatabase
 import no.nav.arbeidsgiver.notifikasjon.util.uuid
-import org.apache.log4j.helpers.ISO8601DateFormat
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.OffsetDateTime
 import java.util.*
 
+private var idsuffixes = generateSequence(0) { it + 1 }.map { it.toString() }.iterator()
+
 class AutoSlettRepositoryTests : DescribeSpec({
     val database = testDatabase(AutoSlett.databaseConfig)
     val repository = AutoSlettRepository(database)
 
-    suspend fun DescribeSpecContainerContext.oppgaveUtførtCase(opprettetTidspunkt: String, opprinneligHardDelete: String, nyHardDelete: String, strategi: HendelseModel.NyTidStrategi, expected: Instant){
-        it("oppgave utført strategi $strategi") {
+    suspend fun DescribeSpecContainerContext.oppgaveUtførtCase(
+        opprettetTidspunkt: String,
+        opprinneligHardDelete: String,
+        nyHardDelete: String,
+        strategi: HendelseModel.NyTidStrategi,
+        expected: String,
+    ) {
+        val nyTid = LocalDateTimeOrDuration.parse(nyHardDelete)
+        val idsuffix = idsuffixes.next()
+        it("oppgave utført ${nyTid.javaClass.simpleName} strategi $strategi") {
             repository.oppgaveOpprettet(
-                idsuffix = "1",
+                idsuffix = idsuffix,
                 opprettetTidspunkt = opprettetTidspunkt,
                 hardDelete = opprinneligHardDelete
             )
             val utførtHendelse = repository.oppgaveUtført(
-                idsuffix = "1",
+                idsuffix = idsuffix,
                 hardDelete = HendelseModel.HardDeleteUpdate(
-                    nyTid = LocalDateTimeOrDuration.parse(nyHardDelete),
+                    nyTid = nyTid,
                     strategi = strategi,
                 )
             )
             it("hard delete scheduleres") {
                 val skedulert = repository.hent(utførtHendelse.aggregateId)
                 skedulert shouldNotBe null
-                skedulert!!.beregnetSlettetidspunkt shouldBe expected
+                skedulert!!.beregnetSlettetidspunkt shouldBe LocalDateTime.parse(expected).atOslo().toInstant()
+            }
+        }
+    }
+
+    suspend fun DescribeSpecContainerContext.sakOppdatertCase(
+        title: String,
+        sakMottattTidspunkt: String,
+        opprinneligHardDelete: String,
+        oppdateringMottattTidspunkt: String,
+        nyHardDelete: String,
+        strategi: HendelseModel.NyTidStrategi,
+        expected: Instant,
+    ) {
+        val nyTid = LocalDateTimeOrDuration.parse(nyHardDelete)
+        val idsuffix = idsuffixes.next()
+        it("ny status sak $title") {
+            repository.sakOpprettet(
+                idsuffix = idsuffix,
+                mottattTidspunkt = sakMottattTidspunkt,
+                hardDelete = opprinneligHardDelete
+            )
+            val nyStatusHendelse = repository.nyStatusSak(
+                idsuffix = idsuffix,
+                mottattTidspunkt = oppdateringMottattTidspunkt,
+                hardDelete = HendelseModel.HardDeleteUpdate(
+                    nyTid = nyTid,
+                    strategi = strategi,
+                )
+            )
+
+            it("hard delete er $expected") {
+                val skedulertHardDelete = repository.hent(nyStatusHendelse.aggregateId)
+                skedulertHardDelete shouldNotBe null
+                skedulertHardDelete!!.beregnetSlettetidspunkt shouldBe expected
             }
         }
     }
@@ -144,7 +188,7 @@ class AutoSlettRepositoryTests : DescribeSpec({
                     "1",
                     hardDelete = HendelseModel.HardDeleteUpdate(
                         nyTid = LocalDateTimeOrDuration.parse("2022-10-13T07:20:50.52"),
-                        strategi = HendelseModel.NyTidStrategi.OVERSKRIV,
+                        strategi = OVERSKRIV,
                     )
                 )
 
@@ -163,7 +207,7 @@ class AutoSlettRepositoryTests : DescribeSpec({
                     mottattTidspunkt = "2020-01-01T01:01+00",
                     hardDelete = HendelseModel.HardDeleteUpdate(
                         nyTid = LocalDateTimeOrDuration.parse("2022-10-13T07:20:50.52"),
-                        strategi = HendelseModel.NyTidStrategi.OVERSKRIV,
+                        strategi = OVERSKRIV,
                     )
                 )
 
@@ -179,12 +223,12 @@ class AutoSlettRepositoryTests : DescribeSpec({
                 opprettetTidspunkt = "2020-01-01T01:01+00",
                 opprinneligHardDelete = "2022-10-13T07:20:50.52",
                 nyHardDelete = "2022-09-13T07:20:50.52",
-                strategi = HendelseModel.NyTidStrategi.OVERSKRIV,
-                LocalDateTime.parse("2022-09-13T07:20:50.52").atOslo().toInstant()
+                strategi = OVERSKRIV,
+                expected = "2022-09-13T07:20:50.52",
             )
 
             //TODO Finne ut av når "om" skal gjelde fra (Hva er basetime?)
-            //  1. Basert på opprettelse.
+            // 1. Basert på opprettelse.
             // 2. Basert på statusoppdatering
             // Begge varianter gir mening.
 
@@ -193,71 +237,76 @@ class AutoSlettRepositoryTests : DescribeSpec({
 //                opprinneligHardDelete = "2022-10-13T07:20:50.52",
 //                nyHardDelete = "P1YT1H",
 //                strategi = HendelseModel.NyTidStrategi.OVERSKRIV,
-//                LocalDateTime.parse("2022-10-13T07:20:50.52").atOslo().toInstant()
+//                expected = "2022-10-13T07:20:50.52",
 //            )
 
             oppgaveUtførtCase(
                 opprettetTidspunkt = "2020-01-01T01:01+00",
                 opprinneligHardDelete = "2022-10-13T07:20:50.52",
                 nyHardDelete = "2022-09-13T07:20:50.52",
-                strategi = HendelseModel.NyTidStrategi.FORLENG,
-                LocalDateTime.parse("2022-10-13T07:20:50.52").atOslo().toInstant()
-
+                strategi = FORLENG,
+                expected = "2022-10-13T07:20:50.52",
             )
 
-            it("ny status sak strategi overskriv") {
-                val opprinneligHardDelete = "2022-10-13T07:20:50.52"
-                val nyHardDelete = "2022-09-13T07:20:50.52"
-                val strategi = HendelseModel.NyTidStrategi.OVERSKRIV
+            sakOppdatertCase(
+                title = "overskriv med absolutt dato",
+                sakMottattTidspunkt = "2020-01-01T01:01+00",
+                opprinneligHardDelete = "2022-10-13T07:20:50.52",
+                oppdateringMottattTidspunkt = "2020-01-01T01:01+00",
+                nyHardDelete = "2022-09-13T07:20:50.52",
+                strategi = OVERSKRIV,
+                expected = LocalDateTime.parse("2022-09-13T07:20:50.52").atOslo().toInstant()
+            )
 
-                repository.sakOpprettet(
-                    idsuffix = "3",
-                    mottattTidspunkt = "2020-01-01T01:01+00",
-                    hardDelete = opprinneligHardDelete
-                )
-                val nyStatusHendelse = repository.nyStatusSak(
-                    idsuffix = "3",
-                    mottattTidspunkt = "2020-01-01T01:01+00",
-                    hardDelete = HendelseModel.HardDeleteUpdate(
-                        nyTid = LocalDateTimeOrDuration.parse(nyHardDelete),
-                        strategi = strategi,
-                    )
-                )
+            sakOppdatertCase(
+                title = "overskriv med offset",
+                sakMottattTidspunkt = "2020-01-01T01:01+00",
+                opprinneligHardDelete = "2022-10-13T07:20:50.52",
+                oppdateringMottattTidspunkt = "2020-01-01T01:01+00",
+                nyHardDelete = "P1YT1H",
+                strategi = OVERSKRIV,
+                expected = OffsetDateTime.parse("2021-01-01T02:01+00").toInstant()
+            )
 
-                it("hard delete scheduleres ikke") {
-                    val skedulertHardDelete = repository.hent(nyStatusHendelse.aggregateId)
-                    skedulertHardDelete shouldNotBe null
-                    skedulertHardDelete!!.beregnetSlettetidspunkt shouldBe LocalDateTime.parse(nyHardDelete).atOslo()
-                        .toInstant()
-                }
-            }
+            sakOppdatertCase(
+                title = "forleng med absolutt dato, men tidligere er eldre",
+                sakMottattTidspunkt = "2020-01-01T01:01+00",
+                opprinneligHardDelete = "2022-10-13T07:20:50.52",
+                oppdateringMottattTidspunkt = "2020-01-01T01:01+00",
+                nyHardDelete = "2022-09-13T07:20:50.52",
+                strategi = FORLENG,
+                expected = LocalDateTime.parse("2022-10-13T07:20:50.52").atOslo().toInstant()
+            )
 
-            it("ny status sak strategi forleng") {
-                val opprinneligHardDelete = "2022-10-13T07:20:50.52"
-                val nyHardDelete = "2022-09-13T07:20:50.52"
-                val strategi = HendelseModel.NyTidStrategi.FORLENG
+            sakOppdatertCase(
+                title = "forleng med absolutt dato",
+                sakMottattTidspunkt = "2020-01-01T01:01+00",
+                opprinneligHardDelete = "2022-10-13T07:20:50.52",
+                oppdateringMottattTidspunkt = "2020-01-01T01:01+00",
+                nyHardDelete = "2023-09-13T07:20:50.52",
+                strategi = FORLENG,
+                expected = LocalDateTime.parse("2023-09-13T07:20:50.52").atOslo().toInstant()
+            )
 
-                repository.sakOpprettet(
-                    idsuffix = "4",
-                    mottattTidspunkt = "2020-01-01T01:01+00",
-                    hardDelete = opprinneligHardDelete
-                )
-                val nyStatusHendelse = repository.nyStatusSak(
-                    idsuffix = "4",
-                    mottattTidspunkt = "2020-01-01T01:01+00",
-                    hardDelete = HendelseModel.HardDeleteUpdate(
-                        nyTid = LocalDateTimeOrDuration.parse(nyHardDelete),
-                        strategi = strategi,
-                    )
-                )
+            sakOppdatertCase(
+                title = "forleng med relativ dato",
+                sakMottattTidspunkt = "2020-01-01T01:01+00",
+                opprinneligHardDelete = "2022-10-13T07:20:50.52",
+                oppdateringMottattTidspunkt = "2020-01-01T01:01+00",
+                nyHardDelete = "P3YT1H",
+                strategi = FORLENG,
+                expected = OffsetDateTime.parse("2023-01-01T02:01+00").toInstant()
+            )
 
-                it("hard delete scheduleres ikke") {
-                    val skedulertHardDelete = repository.hent(nyStatusHendelse.aggregateId)
-                    skedulertHardDelete shouldNotBe null
-                    skedulertHardDelete!!.beregnetSlettetidspunkt shouldBe LocalDateTime.parse(opprinneligHardDelete)
-                        .atOslo().toInstant()
-                }
-            }
+            sakOppdatertCase(
+                title = "forleng med relativ dato, men tidligere er eldre",
+                sakMottattTidspunkt = "2020-01-01T01:01+00",
+                opprinneligHardDelete = "2022-10-13T07:20:50.52",
+                oppdateringMottattTidspunkt = "2020-01-01T01:01+00",
+                nyHardDelete = "P1YT1H",
+                strategi = FORLENG,
+                expected = LocalDateTime.parse("2022-10-13T07:20:50.52").atOslo().toInstant()
+            )
         }
     }
 })
