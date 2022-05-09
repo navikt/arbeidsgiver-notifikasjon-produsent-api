a4bfb4548dd83656430bc79c7d61b519a94e2212
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-05-09 15:33:03 +0200

    Toplevel exception logger

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/Main.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/Main.kt
@@ -1,6 +1,7 @@
 package no.nav.arbeidsgiver.notifikasjon
 
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.logger
+import kotlin.system.exitProcess
 
 @Suppress("unused") /* Entry point for jar. */
 private object Main {
@@ -8,15 +9,20 @@ private object Main {
 }
 
 fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
-    when (val navn = System.getenv("NAIS_APP_NAME")) {
-        "notifikasjon-produsent-api" -> Produsent.main()
-        "notifikasjon-bruker-api" -> Bruker.main()
-        "notifikasjon-kafka-reaper" -> KafkaReaper.main()
-        "notifikasjon-statistikk" -> Statistikk.main()
-        "notifikasjon-ekstern-varsling" -> EksternVarsling.main()
-        "notifikasjon-replay-validator" -> ReplayValidator.main()
-        "notifikasjon-autoslett" -> AutoSlett.main()
-        else -> Main.log.error("ukjent \$NAIS_APP_NAME '$navn'")
+    try {
+        when (val navn = System.getenv("NAIS_APP_NAME")) {
+            "notifikasjon-produsent-api" -> Produsent.main()
+            "notifikasjon-bruker-api" -> Bruker.main()
+            "notifikasjon-kafka-reaper" -> KafkaReaper.main()
+            "notifikasjon-statistikk" -> Statistikk.main()
+            "notifikasjon-ekstern-varsling" -> EksternVarsling.main()
+            "notifikasjon-replay-validator" -> ReplayValidator.main()
+            "notifikasjon-autoslett" -> AutoSlett.main()
+            else -> Main.log.error("ukjent \$NAIS_APP_NAME '$navn'")
+        }
+    } catch (e: Exception) {
+        Main.log.error("unhandled toplevel exception {}. exiting.", e.message, e)
+        exitProcess(1)
     }
 }
 
