51f34b0b3923af90595ff4f63fb24ab4db7a9ee6
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-10-13 09:28:50 +0200

    remove unused imports

--- app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/infrastruktur/PropagateFromMDCPluginTests.kt
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/infrastruktur/PropagateFromMDCPluginTests.kt
@@ -3,10 +3,6 @@ package no.nav.arbeidsgiver.notifikasjon.infrastruktur
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.ktor.client.*
-import io.ktor.client.call.body
-import io.ktor.client.call.body
-import io.ktor.client.call.body
-import io.ktor.client.call.body
 import io.ktor.client.engine.mock.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
