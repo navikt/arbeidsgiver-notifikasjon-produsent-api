0981e5dccd150979e0d37931fbb0efd4f8f07705
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-11-03 09:45:34 +0100

    database alltid klar når man ikke har database

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_utgått/SkedulertUgått.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_utgått/SkedulertUgått.kt
@@ -2,10 +2,14 @@ package no.nav.arbeidsgiver.notifikasjon.skedulert_utgått
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Health
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Subsystem
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.http.launchHttpServer
 
 object SkedulertUgått {
     fun main(httpPort: Int = 8080) {
+        Health.subsystemReady[Subsystem.DATABASE] = true
+
         runBlocking(Dispatchers.Default) {
             launchHttpServer(httpPort = httpPort)
         }
