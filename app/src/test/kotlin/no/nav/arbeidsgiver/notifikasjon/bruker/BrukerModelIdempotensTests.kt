a5342716f1909ac638ee623663ea54562911877c
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-02-06 11:38:10 +0100

    unit-test for duplikat-håndtering av mottaker i bruker-api

--- app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/BrukerModelIdempotensTests.kt
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/BrukerModelIdempotensTests.kt
@@ -2,6 +2,7 @@ package no.nav.arbeidsgiver.notifikasjon.bruker
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
 import no.nav.arbeidsgiver.notifikasjon.util.EksempelHendelse
 import no.nav.arbeidsgiver.notifikasjon.util.testDatabase
 
@@ -13,6 +14,20 @@ class BrukerModelIdempotensTests : DescribeSpec({
         withData(EksempelHendelse.Alle) { hendelse ->
             queryModel.oppdaterModellEtterHendelse(hendelse)
             queryModel.oppdaterModellEtterHendelse(hendelse)
+        }
+        context("NyBeskjed to ganger") {
+            queryModel.oppdaterModellEtterHendelse(EksempelHendelse.BeskjedOpprettet)
+            queryModel.oppdaterModellEtterHendelse(EksempelHendelse.BeskjedOpprettet)
+
+            it("ingen duplikat mottaker") {
+                val antallMottakere = database.nonTransactionalExecuteQuery("""
+                select * from mottaker_altinn_enkeltrettighet
+                where notifikasjon_id = '${EksempelHendelse.BeskjedOpprettet.notifikasjonId}'
+            """
+                ) {
+                }.size
+                antallMottakere shouldBe 1
+            }
         }
     }
 })
