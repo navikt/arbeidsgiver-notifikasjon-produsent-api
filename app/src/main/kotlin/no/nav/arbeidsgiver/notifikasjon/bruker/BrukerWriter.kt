23b5254b7d116e61899ad47ed8cd715f8372c388
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-02-10 14:21:48 +0100

    move processing from api to worker

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/BrukerWriter.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/BrukerWriter.kt
@@ -4,21 +4,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import no.nav.arbeidsgiver.notifikasjon.altinn_roller.AltinnRolleClient
-import no.nav.arbeidsgiver.notifikasjon.altinn_roller.AltinnRolleClientImpl
-import no.nav.arbeidsgiver.notifikasjon.altinn_roller.AltinnRolleService
-import no.nav.arbeidsgiver.notifikasjon.altinn_roller.AltinnRolleServiceImpl
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Database
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Database.Companion.openDatabaseAsync
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Health
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Subsystem
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.http.launchHttpServer
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.HendelsesstrømKafkaImpl
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.NOTIFIKASJON_TOPIC
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.NærmesteLederKafkaListener
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.launchProcessingLoop
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.logger
-import java.time.Duration
 
 object BrukerWriter {
     private val log = logger()
@@ -30,14 +22,14 @@ object BrukerWriter {
             "cloudSqlInstance" to System.getenv("CLOUD_SQL_INSTANCE")!!
         )
     )
-//
-//    private val hendelsesstrøm by lazy {
-//        HendelsesstrømKafkaImpl(
-//            topic = NOTIFIKASJON_TOPIC,
-//            groupId = "bruker-model-builder-2",
-//            replayPeriodically = true,
-//        )
-//    }
+
+    private val hendelsesstrøm by lazy {
+        HendelsesstrømKafkaImpl(
+            topic = NOTIFIKASJON_TOPIC,
+            groupId = "bruker-model-builder-2",
+            replayPeriodically = true,
+        )
+    }
 
     fun main(
 //        altinnRolleClient: AltinnRolleClient = AltinnRolleClientImpl(),
@@ -54,19 +46,19 @@ object BrukerWriter {
                 log.info("successfully connected to database, it seems")
             }
 
-//            launch {
-//                val brukerRepository = brukerRepositoryAsync.await()
-//                hendelsesstrøm.forEach { event ->
-//                    brukerRepository.oppdaterModellEtterHendelse(event)
-//                }
-//            }
-//
-//            launch {
-//                val brukerRepository = brukerRepositoryAsync.await()
-//                NærmesteLederKafkaListener().forEach { event ->
-//                    brukerRepository.oppdaterModellEtterNærmesteLederLeesah(event)
-//                }
-//            }
+            launch {
+                val brukerRepository = brukerRepositoryAsync.await()
+                hendelsesstrøm.forEach { event ->
+                    brukerRepository.oppdaterModellEtterHendelse(event)
+                }
+            }
+
+            launch {
+                val brukerRepository = brukerRepositoryAsync.await()
+                NærmesteLederKafkaListener().forEach { event ->
+                    brukerRepository.oppdaterModellEtterNærmesteLederLeesah(event)
+                }
+            }
 //
 //            val altinnRolleService = async<AltinnRolleService> {
 //                AltinnRolleServiceImpl(altinnRolleClient, brukerRepositoryAsync.await().altinnRolle)
