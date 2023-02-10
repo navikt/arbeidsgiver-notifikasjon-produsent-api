3eb8f8c9dbfea8ff831cbfe75d57adb717355e20
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-02-10 16:31:40 +0100

    tilgangsstyring er nå endret

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/BrukerWriter.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/BrukerWriter.kt
@@ -48,13 +48,13 @@ object BrukerWriter {
                 }
             }
 
-//            launch {
-//                val brukerRepository = brukerRepositoryAsync.await()
-//                NærmesteLederKafkaListener().forEach { event ->
-//                    brukerRepository.oppdaterModellEtterNærmesteLederLeesah(event)
-//                }
-//            }
-//
+            launch {
+                val brukerRepository = brukerRepositoryAsync.await()
+                NærmesteLederKafkaListener().forEach { event ->
+                    brukerRepository.oppdaterModellEtterNærmesteLederLeesah(event)
+                }
+            }
+
 //            val altinnRolleService = async<AltinnRolleService> {
 //                AltinnRolleServiceImpl(altinnRolleClient, brukerRepositoryAsync.await().altinnRolle)
 //            }
