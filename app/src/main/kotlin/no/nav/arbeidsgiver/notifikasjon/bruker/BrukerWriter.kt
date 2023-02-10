3582b3396fc7f0a407cdc65ff44000b31a32616f
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-02-10 12:55:47 +0100

    skeleton for bruker-api-writer

---
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/BrukerWriter.kt
@@ -0,0 +1,73 @@
+package no.nav.arbeidsgiver.notifikasjon.bruker
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import no.nav.arbeidsgiver.notifikasjon.altinn_roller.AltinnRolleClient
+import no.nav.arbeidsgiver.notifikasjon.altinn_roller.AltinnRolleClientImpl
+import no.nav.arbeidsgiver.notifikasjon.altinn_roller.AltinnRolleService
+import no.nav.arbeidsgiver.notifikasjon.altinn_roller.AltinnRolleServiceImpl
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Database
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Database.Companion.openDatabaseAsync
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.http.launchHttpServer
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.HendelsesstrømKafkaImpl
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.NOTIFIKASJON_TOPIC
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.NærmesteLederKafkaListener
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.launchProcessingLoop
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.logger
+import java.time.Duration
+
+object BrukerWriter {
+    private val log = logger()
+//    val databaseConfig = Database.config("bruker_model")
+//
+//    private val hendelsesstrøm by lazy {
+//        HendelsesstrømKafkaImpl(
+//            topic = NOTIFIKASJON_TOPIC,
+//            groupId = "bruker-model-builder-2",
+//            replayPeriodically = true,
+//        )
+//    }
+
+    fun main(
+//        altinnRolleClient: AltinnRolleClient = AltinnRolleClientImpl(),
+        httpPort: Int = 8080
+    ) {
+        log.info("hello world")
+        runBlocking(Dispatchers.Default) {
+//            val database = openDatabaseAsync(databaseConfig)
+//            val brukerRepositoryAsync = async {
+//                BrukerRepositoryImpl(database.await())
+//            }
+//
+//            launch {
+//                val brukerRepository = brukerRepositoryAsync.await()
+//                hendelsesstrøm.forEach { event ->
+//                    brukerRepository.oppdaterModellEtterHendelse(event)
+//                }
+//            }
+//
+//            launch {
+//                val brukerRepository = brukerRepositoryAsync.await()
+//                NærmesteLederKafkaListener().forEach { event ->
+//                    brukerRepository.oppdaterModellEtterNærmesteLederLeesah(event)
+//                }
+//            }
+//
+//            val altinnRolleService = async<AltinnRolleService> {
+//                AltinnRolleServiceImpl(altinnRolleClient, brukerRepositoryAsync.await().altinnRolle)
+//            }
+//
+//
+//            launchProcessingLoop(
+//                "last Altinnroller",
+//                pauseAfterEach = Duration.ofDays(1),
+//            ) {
+//                altinnRolleService.await().lastRollerFraAltinn()
+//            }
+
+            launchHttpServer(httpPort = httpPort)
+        }
+    }
+}
