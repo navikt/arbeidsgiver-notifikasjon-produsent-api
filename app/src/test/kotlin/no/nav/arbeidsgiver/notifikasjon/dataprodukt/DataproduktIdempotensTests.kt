4f7a14fb77effb57b1c2f2017e48ec878731ab18
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-02-20 16:00:27 +0100

    Idempotens-sjekk for dataprodukt-model

---
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/dataprodukt/DataproduktIdempotensTests.kt
@@ -0,0 +1,25 @@
+package no.nav.arbeidsgiver.notifikasjon.dataprodukt
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.datatest.withData
+import no.nav.arbeidsgiver.notifikasjon.hendelse.HendelseModel
+import no.nav.arbeidsgiver.notifikasjon.hendelse.HendelseModel.HendelseMetadata
+import no.nav.arbeidsgiver.notifikasjon.hendelse.HendelseProdusent
+import no.nav.arbeidsgiver.notifikasjon.util.EksempelHendelse
+import no.nav.arbeidsgiver.notifikasjon.util.NoopHendelseProdusent
+import no.nav.arbeidsgiver.notifikasjon.util.testDatabase
+import java.time.Instant
+
+class DataproduktIdempotensTests : DescribeSpec({
+    val database = testDatabase(Dataprodukt.databaseConfig)
+    val service = DataproduktModel(database)
+
+    val now = Instant.now()
+
+    describe("Dataprodukt Idempotent oppførsel") {
+        withData(EksempelHendelse.Alle) { hendelse ->
+            service.oppdaterModellEtterHendelse(hendelse, HendelseMetadata(now))
+            service.oppdaterModellEtterHendelse(hendelse, HendelseMetadata(now))
+        }
+    }
+})
