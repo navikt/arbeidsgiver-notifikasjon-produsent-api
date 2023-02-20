127da6f436fa0895e52814363d839a7796f35a46
Ken Gullaksen <ken.gullaksen@nav.no>
2023-02-20 08:41:58 +0100

    reenable tokenx cache

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/infrastruktur/tokenx/TokenXClient.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/infrastruktur/tokenx/TokenXClient.kt
@@ -1,6 +1,8 @@
 package no.nav.arbeidsgiver.notifikasjon.infrastruktur.tokenx
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.github.benmanes.caffeine.cache.Caffeine
+import com.github.benmanes.caffeine.cache.Expiry
 import com.nimbusds.jose.JOSEObjectType
 import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.JWSHeader
@@ -9,25 +11,25 @@ import com.nimbusds.jose.crypto.RSASSASigner
 import com.nimbusds.jose.jwk.RSAKey
 import com.nimbusds.jwt.JWTClaimsSet
 import com.nimbusds.jwt.SignedJWT
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.engine.apache.Apache
-import io.ktor.client.plugins.HttpRequestRetry
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.accept
-import io.ktor.client.request.forms.FormDataContent
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.http.ContentType
-import io.ktor.http.Parameters
-import io.ktor.serialization.jackson.jackson
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.http.*
+import io.ktor.serialization.jackson.*
 import io.micrometer.core.instrument.Counter
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.HttpClientMetricsFeature
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Metrics
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.altinn.getAsync
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.logger
 import org.apache.http.ConnectionClosedException
 import java.time.Instant
 import java.util.*
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.ExperimentalTime
 
 fun defaultHttpClient() = HttpClient(Apache) {
     install(ContentNegotiation) {
@@ -64,34 +66,34 @@ class TokenXClientImpl(
         .type(JOSEObjectType.JWT)
         .build()
 
-//    private data class CacheKey(
-//        private val audience: String,
-//        private val subjectToken: String,
-//    )
-//
-//    @OptIn(ExperimentalTime::class)
-//    private val exchangeCache = Caffeine.newBuilder()
-//        .maximumSize(5_000L)
-//        .expireAfter(object : Expiry<CacheKey, AccessToken> {
-//            override fun expireAfterCreate(key: CacheKey, response: AccessToken, currentTime: Long) =
-//                (response.expires_in.seconds - 5.seconds).inWholeNanoseconds
-//
-//            override fun expireAfterUpdate(
-//                key: CacheKey,
-//                value: AccessToken,
-//                currentTime: Long,
-//                currentDuration: Long
-//            ) = currentDuration
-//
-//            override fun expireAfterRead(
-//                key: CacheKey,
-//                value: AccessToken,
-//                currentTime: Long,
-//                currentDuration: Long
-//            ) = currentDuration
-//        })
-//        .buildAsync<CacheKey, AccessToken>()
-//
+    private data class CacheKey(
+        private val audience: String,
+        private val subjectToken: String,
+    )
+
+    @OptIn(ExperimentalTime::class)
+    private val exchangeCache = Caffeine.newBuilder()
+        .maximumSize(10_000L)
+        .expireAfter(object : Expiry<CacheKey, AccessToken> {
+            override fun expireAfterCreate(key: CacheKey, response: AccessToken, currentTime: Long) =
+                (response.expires_in.seconds - 5.seconds).inWholeNanoseconds
+
+            override fun expireAfterUpdate(
+                key: CacheKey,
+                value: AccessToken,
+                currentTime: Long,
+                currentDuration: Long
+            ) = currentDuration
+
+            override fun expireAfterRead(
+                key: CacheKey,
+                value: AccessToken,
+                currentTime: Long,
+                currentDuration: Long
+            ) = currentDuration
+        })
+        .buildAsync<CacheKey, AccessToken>()
+
     private val log = logger()
     private val successCounter = Counter.builder("tokenx.success")
         .register(Metrics.meterRegistry)
@@ -99,35 +101,37 @@ class TokenXClientImpl(
         .register(Metrics.meterRegistry)
 
     override suspend fun exchange(subjectToken: String, audience: String): String {
-        try {
-            val assertion = makeClientAssertion()
+        val accessTokenEntry = exchangeCache.getAsync(CacheKey(audience, subjectToken)) {
+            try {
+                val assertion = makeClientAssertion()
 
-            return httpClient.post(config.tokenEndpoint) {
-                accept(ContentType.Application.Json)
-                setBody(FormDataContent(
-                    Parameters.build {
-                        append("grant_type", "urn:ietf:params:oauth:grant-type:token-exchange")
-                        append(
-                            "client_assertion_type",
-                            "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
-                        )
-                        append("client_assertion", assertion)
-                        append("subject_token_type", "urn:ietf:params:oauth:token-type:jwt")
-                        append("subject_token", subjectToken)
-                        append("audience", audience)
-                    }
-                ))
-            }
-                .body<AccessToken>()
-                .also {
-                    successCounter.increment()
+                httpClient.post(config.tokenEndpoint) {
+                    accept(ContentType.Application.Json)
+                    setBody(FormDataContent(
+                        Parameters.build {
+                            append("grant_type", "urn:ietf:params:oauth:grant-type:token-exchange")
+                            append(
+                                "client_assertion_type",
+                                "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
+                            )
+                            append("client_assertion", assertion)
+                            append("subject_token_type", "urn:ietf:params:oauth:token-type:jwt")
+                            append("subject_token", subjectToken)
+                            append("audience", audience)
+                        }
+                    ))
                 }
-                .access_token
-        } catch (e: RuntimeException) {
-            log.info("token exchange failed (audience='{}').", audience, e)
-            failCounter.increment()
-            throw e
+                    .body<AccessToken>()
+                    .also {
+                        successCounter.increment()
+                    }
+            } catch (e: RuntimeException) {
+                log.info("token exchange failed (audience='{}').", audience, e)
+                failCounter.increment()
+                throw e
+            }
         }
+        return accessTokenEntry.access_token
     }
 
 
