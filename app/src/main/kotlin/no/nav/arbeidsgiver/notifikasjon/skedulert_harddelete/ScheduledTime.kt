18227cc2f97feb10af922bcf78bd49acaffd0f2e
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-02-21 11:17:16 +0100

    Instant + Period with Years not supported

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_harddelete/ScheduledTime.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_harddelete/ScheduledTime.kt
@@ -3,6 +3,7 @@ package no.nav.arbeidsgiver.notifikasjon.skedulert_harddelete
 import no.nav.arbeidsgiver.notifikasjon.hendelse.HendelseModel.LocalDateTimeOrDuration
 import no.nav.arbeidsgiver.notifikasjon.tid.atOslo
 import java.time.Instant
+import java.time.ZoneOffset
 
 class ScheduledTime(
     private val spec: LocalDateTimeOrDuration,
@@ -10,7 +11,9 @@ class ScheduledTime(
 ) {
     fun happensAt(): Instant = when (spec) {
         is LocalDateTimeOrDuration.LocalDateTime -> spec.value.atOslo().toInstant()
-        is LocalDateTimeOrDuration.Duration -> baseTime + spec.value
+        is LocalDateTimeOrDuration.Duration ->
+            /* adding years is not supported by Instant. */
+            (baseTime.atOffset(ZoneOffset.UTC) + spec.value).toInstant()
     }
 
     fun omOrNull() = spec.omOrNull()
