f8414544b039af643de8368a19960b1f070175f6
Ken Gullaksen <ken.gullaksen@nav.no>
2023-03-06 16:11:07 +0100

    prepare for production

--- terraform/dev.tfvars
+++ terraform/dev.tfvars
@@ -1,2 +1,1 @@
 project = "fager-dev-24f2"
-region  = "europe-north1"
--- terraform/vaiables.tf
+++ terraform/vaiables.tf
@@ -8,8 +8,3 @@ variable "project" {
   type        = string
   description = "Project"
 }
-
-variable "vm_zone" {
-  type    = string
-  default = "europe-north1-a"
-}
