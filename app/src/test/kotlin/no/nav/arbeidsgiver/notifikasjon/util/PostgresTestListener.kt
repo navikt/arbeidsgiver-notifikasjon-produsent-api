cd6bac3fe868402dc1f844721f3a46eabb8d8d2d
Ken Gullaksen <ken.gullaksen@nav.no>
2023-03-16 20:01:57 +0100

    wip parallell kotest

--- app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/util/PostgresTestListener.kt
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/util/PostgresTestListener.kt
@@ -6,14 +6,29 @@ import io.kotest.core.spec.Spec
 import io.kotest.core.test.TestCase
 import kotlinx.coroutines.runBlocking
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Database
+import java.sql.DriverManager
+import java.sql.SQLException
+import java.util.*
 
+val ids = generateSequence() { UUID.randomUUID() }.iterator()
 fun TestConfiguration.testDatabase(config: Database.Config): Database =
     runBlocking {
+        val db = "${config.database}_${ids.next()}"
+        try {
+            DriverManager.getConnection(config.jdbcUrl, config.username, config.password).use { conn ->
+                conn.createStatement().use { stmt ->
+                    stmt.executeUpdate("""create database "$db"; """)
+                }
+            }
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        }
         Database.openDatabase(
             config.copy(
                 // https://github.com/flyway/flyway/issues/2323#issuecomment-804495818
                 jdbcOpts = mapOf("preparedStatementCacheQueries" to 0),
                 port = "1337",
+                database = db,
             )
         )
     }
