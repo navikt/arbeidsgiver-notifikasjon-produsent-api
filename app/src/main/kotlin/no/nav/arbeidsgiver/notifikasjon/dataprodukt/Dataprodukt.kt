09670edffd82a04430b536c78e10c5da96409424
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-03-20 11:20:40 +0100

    stop processing until we empty database

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/dataprodukt/Dataprodukt.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/dataprodukt/Dataprodukt.kt
@@ -11,24 +11,27 @@ import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.NOTIFIKASJON_TOPIC
 
 object Dataprodukt {
     val databaseConfig = Database.config("dataprodukt_model")
-    private val hendelsesstrøm by lazy {
-        HendelsesstrømKafkaImpl(
-            topic = NOTIFIKASJON_TOPIC,
-            groupId = "dataprodukt-model-builder-2",
-            replayPeriodically = true,
-        )
-    }
+//    private val hendelsesstrøm by lazy {
+//        HendelsesstrømKafkaImpl(
+//            topic = NOTIFIKASJON_TOPIC,
+//            groupId = "dataprodukt-model-builder-2",
+//            replayPeriodically = true,
+//        )
+//    }
 
     fun main(httpPort: Int = 8080) {
         runBlocking(Dispatchers.Default) {
             val database = openDatabaseAsync(databaseConfig)
 
             launch {
-                val dataproduktModel = DataproduktModel(database.await())
-                hendelsesstrøm.forEach { hendelse, metadata ->
-                    dataproduktModel.oppdaterModellEtterHendelse(hendelse, metadata)
-                }
+                database.await()
             }
+//            launch {
+//                val dataproduktModel = DataproduktModel(database.await())
+//                hendelsesstrøm.forEach { hendelse, metadata ->
+//                    dataproduktModel.oppdaterModellEtterHendelse(hendelse, metadata)
+//                }
+//            }
 
             launchHttpServer(httpPort = httpPort)
         }
