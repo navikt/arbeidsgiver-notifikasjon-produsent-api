e8b2a042400f5b13ce2deeb32244f3730ac4e839
Ken Gullaksen <ken.gullaksen@nav.no>
2023-03-28 11:58:08 +0200

    alter defaults fails to deploy, remove for now

--- app/src/main/resources/db/migration/bruker_model/V28__access_to_hard_deleted_aggregates.sql
+++ app/src/main/resources/db/migration/bruker_model/V28__access_to_hard_deleted_aggregates.sql
@@ -1,9 +1,3 @@
-alter default privileges for user "notifikasjon-bruker-api" in schema public grant all on tables to cloudsqliamuser;
-alter default privileges for user "notifikasjon-bruker-api" in schema public grant all on tables to "bruker-api-kafka-user";
-
-alter default privileges for user "bruker-api-kafka-user" in schema public grant all on tables to cloudsqliamuser;
-alter default privileges for user "bruker-api-kafka-user" in schema public grant all on tables to "notifikasjon-bruker-api";
-
 grant all privileges on schema public to "notifikasjon-bruker-api";
 grant all privileges on schema public to "bruker-api-kafka-user";
 grant all privileges on schema public to cloudsqliamuser;
