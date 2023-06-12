402e0bc703797f79e86dcbe986f4edabf59e657b
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-06-12 23:17:18 +0200

    Remove debug info, as problem was found

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/Bruker.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/Bruker.kt
@@ -1,6 +1,5 @@
 package no.nav.arbeidsgiver.notifikasjon.bruker
 
-import io.ktor.server.metrics.micrometer.*
 import io.micrometer.core.instrument.Tags
 import io.micrometer.core.instrument.search.MeterNotFoundException
 import kotlinx.coroutines.*
@@ -17,8 +16,6 @@ import no.nav.arbeidsgiver.notifikasjon.infrastruktur.http.extractBrukerContext
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.http.launchGraphqlServer
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.NOTIFIKASJON_TOPIC
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.lagKafkaHendelseProdusent
-import java.io.File
-import java.io.FileWriter
 import java.time.Duration
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -71,21 +68,6 @@ object Bruker {
             launchProcessingLoop("debug coroutines", pauseAfterEach = Duration.ofMinutes(1)) {
                 DebugProbes.dumpCoroutinesInfo().groupBy { it.state }.forEach { (state, coroutines) ->
                     coroutineGauges[state]?.set(coroutines.size)
-                }
-            }
-
-            launchProcessingLoop("debug threads", pauseAfterEach = Duration.ofMinutes(10)) {
-                val allThreads = Thread.getAllStackTraces()
-                val stringBuilder = StringBuilder()
-                for ((thread, stackTrace) in allThreads) {
-                    stringBuilder.append("Thread ${thread.name} (state=${thread.state}):\n")
-                    for (line in stackTrace) {
-                        stringBuilder.append(" $line\n")
-                    }
-                    stringBuilder.append("\n")
-                }
-                FileWriter("/tmp/threads.txt", false).use { file ->
-                    file.write(stringBuilder.toString())
                 }
             }
 
