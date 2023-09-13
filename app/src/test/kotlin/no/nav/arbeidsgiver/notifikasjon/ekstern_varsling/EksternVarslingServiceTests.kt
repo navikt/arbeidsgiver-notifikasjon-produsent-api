82d2f4feca028748a0b9a55059475a2aa99b5cbe
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-09-13 08:51:19 +0200

    unit test: vent lengere på svar fra kafka

--- app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/ekstern_varsling/EksternVarslingServiceTests.kt
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/ekstern_varsling/EksternVarslingServiceTests.kt
@@ -102,7 +102,7 @@ class EksternVarslingServiceTests : DescribeSpec({
             }
 
             it("message received from kafka") {
-                eventually(10.seconds) {
+                eventually(20.seconds) {
                     val vellykedeVarsler = hendelseProdusent.hendelserOfType<EksterntVarselVellykket>()
                     vellykedeVarsler shouldNot beEmpty()
                 }
