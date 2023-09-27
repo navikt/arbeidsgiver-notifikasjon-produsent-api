8c41e76143c184fd6008b178f26333bacaeac818
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-09-27 15:06:23 +0200

    Skjedulert påminnelse: håndter flere bestillinger på samme aggregat

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_påminnelse/SkedulertPåminnelseRepository.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_påminnelse/SkedulertPåminnelseRepository.kt
@@ -1,32 +1,39 @@
 package no.nav.arbeidsgiver.notifikasjon.skedulert_påminnelse
 
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import no.nav.arbeidsgiver.notifikasjon.hendelse.HendelseModel
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.unblocking.MutexProtectedValue
 import java.time.Instant
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 import java.util.*
 
+private typealias OppgaveId = UUID
+private typealias BestillingHendelseId = UUID
+
 class SkedulertPåminnelseRepository {
-    private val mutex = Mutex()
-    private val indexedLookup = HashMap<UUID, SkedulertPåminnelse>()
-    private val påminnelseQueue = TreeMap<Instant, MutableList<SkedulertPåminnelse>>()
+    private class State {
+        val oppgaveIdIndex = HashMap<OppgaveId, MutableList<SkedulertPåminnelse>>()
+        val bestillingsIdIndex = HashMap<BestillingHendelseId, SkedulertPåminnelse>()
+        val påminnelseQueue = TreeMap<Instant, MutableList<SkedulertPåminnelse>>()
+    }
+
+    private val state = MutexProtectedValue { State() }
 
     data class SkedulertPåminnelse(
-        val oppgaveId: UUID,
+        val oppgaveId: OppgaveId,
         val fristOpprettetTidspunkt: Instant,
         val frist: LocalDate?,
         val tidspunkt: HendelseModel.PåminnelseTidspunkt,
         val eksterneVarsler: List<HendelseModel.EksterntVarsel>,
         val virksomhetsnummer: String,
         val produsentId: String,
+        val bestillingHendelseId: BestillingHendelseId,
     ) {
         val queueKey: Instant = tidspunkt.påminnelseTidspunkt.truncatedTo(ChronoUnit.HOURS)
     }
 
     suspend fun hentOgFjernAlleAktuellePåminnelser(now: Instant): Collection<SkedulertPåminnelse> =
-        mutex.withLock {
+        state.withLockApply {
             val alleAktuelle = mutableListOf<SkedulertPåminnelse>()
             val harPassert = { it: SkedulertPåminnelse -> it.tidspunkt.påminnelseTidspunkt < now }
 
@@ -35,37 +42,56 @@ class SkedulertPåminnelseRepository {
                 && påminnelseQueue.firstKey() < now
                 && påminnelseQueue.firstEntry().value.any(harPassert)
             ) {
-                val (key, potensiellePåminnelser) = påminnelseQueue.firstEntry()
+                val (_, potensiellePåminnelser) = påminnelseQueue.firstEntry()
                 val aktuellePåminnelser = potensiellePåminnelser.filter(harPassert)
-
-                potensiellePåminnelser.removeAll(aktuellePåminnelser)
-                if (potensiellePåminnelser.isEmpty()) {
-                    påminnelseQueue.remove(key)
-                }
-
-                aktuellePåminnelser.forEach {
-                    indexedLookup.remove(it.oppgaveId)
-                }
-
                 alleAktuelle.addAll(aktuellePåminnelser)
+                remove(aktuellePåminnelser)
             }
-
-            return@withLock alleAktuelle
+            return@withLockApply alleAktuelle
         }
 
 
-    suspend fun add(t: SkedulertPåminnelse): Unit = mutex.withLock {
-        indexedLookup[t.oppgaveId] = t
-        val alleMedSammeFrist = påminnelseQueue.computeIfAbsent(t.queueKey) { mutableListOf() }
-        alleMedSammeFrist.add(t)
+    suspend fun add(t: SkedulertPåminnelse): Unit = state.withLockApply {
+        bestillingsIdIndex[t.bestillingHendelseId] = t
+        oppgaveIdIndex.computeIfAbsent(t.oppgaveId) { mutableListOf() }.add(t)
+        påminnelseQueue.computeIfAbsent(t.queueKey) { mutableListOf() }.add(t)
     }
 
-    suspend fun remove(id: UUID) = mutex.withLock {
-        val removed = indexedLookup.remove(id) ?: return@withLock
-        val alleMedSammePåminnelseTidspunkt = påminnelseQueue[removed.queueKey] ?: return@withLock
-        alleMedSammePåminnelseTidspunkt.removeIf { it.oppgaveId == id }
+    suspend fun removeOppgaveId(oppgaveId: OppgaveId) = state.withLockApply {
+        oppgaveIdIndex[oppgaveId]?.let {
+            remove(it)
+        }
+    }
+
+    suspend fun removeBestillingId(bestillingId: BestillingHendelseId) = state.withLockApply {
+        bestillingsIdIndex[bestillingId]?.let {
+            remove(it)
+        }
+    }
+
+    private fun State.remove(skjedulertePåminnelser: Iterable<SkedulertPåminnelseRepository.SkedulertPåminnelse>) {
+        /* Duplicate list, because underlying list is modified by call to `remove`. */
+        for (skjedulertPåminnelse in skjedulertePåminnelser.toList()) {
+            remove(skjedulertPåminnelse)
+        }
+    }
+
+    private fun State.remove(skedulertPåminelse: SkedulertPåminnelseRepository.SkedulertPåminnelse) {
+        bestillingsIdIndex.remove(skedulertPåminelse.bestillingHendelseId)
+
+        val oppgaveBestillinger = oppgaveIdIndex[skedulertPåminelse.oppgaveId]
+        if (oppgaveBestillinger != null) {
+            oppgaveBestillinger.removeIf { it.bestillingHendelseId == skedulertPåminelse.bestillingHendelseId }
+            if (oppgaveBestillinger.isEmpty()) {
+                oppgaveIdIndex.remove(skedulertPåminelse.oppgaveId)
+            }
+        }
+
+        val alleMedSammePåminnelseTidspunkt = påminnelseQueue[skedulertPåminelse.queueKey] ?: return
+        alleMedSammePåminnelseTidspunkt.removeIf { it.bestillingHendelseId == skedulertPåminelse.bestillingHendelseId }
         if (alleMedSammePåminnelseTidspunkt.isEmpty()) {
-            påminnelseQueue.remove(removed.queueKey)
+            påminnelseQueue.remove(skedulertPåminelse.queueKey)
         }
     }
 }
+
