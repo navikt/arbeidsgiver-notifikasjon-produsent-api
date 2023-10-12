77c633e29c884977dc1d283cb6c5206822e2646b
Ken Gullaksen <ken.gullaksen@nav.no>
2023-10-12 08:14:58 +0200

    fjern default fra call site

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/ekstern_varsling/EksternVarslingService.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/ekstern_varsling/EksternVarslingService.kt
@@ -203,7 +203,7 @@ class EksternVarslingService(
         withContext(varsel.asMDCContext()) {
             when (varsel) {
                 is EksternVarselTilstand.Ny -> {
-                    val kalkulertSendeTidspunkt = varsel.kalkuertSendetidspunkt(OsloTid.localDateTimeNow())
+                    val kalkulertSendeTidspunkt = varsel.kalkuertSendetidspunkt()
                     if (kalkulertSendeTidspunkt <= OsloTid.localDateTimeNow()) {
                         when (val response = altinnVarselKlient.send(varsel.data.eksternVarsel)) {
                             is AltinnVarselKlientResponse.Ok -> {
