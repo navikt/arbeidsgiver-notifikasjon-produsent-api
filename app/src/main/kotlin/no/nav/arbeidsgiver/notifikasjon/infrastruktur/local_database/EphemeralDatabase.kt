85917847822a7c268b32a698b9bfa97d87d5fe90
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-11-02 12:09:32 +0100

    Introduserer hjelpemetoder for sqlite

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/infrastruktur/local_database/EphemeralDatabase.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/infrastruktur/local_database/EphemeralDatabase.kt
@@ -1,11 +1,18 @@
 package no.nav.arbeidsgiver.notifikasjon.infrastruktur.local_database
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.zaxxer.hikari.HikariDataSource
 import org.intellij.lang.annotations.Language
 import java.io.File
 import java.sql.Connection
 import java.sql.PreparedStatement
 import java.sql.ResultSet
+import java.time.Instant
+import java.time.LocalDate
+import java.util.*
 
 /** Non-persisted, local database. Each instantiation of `EphemeralDatabase`
  * creates a fresh database, independent of all other `EphemeralDatabase`-instances.
@@ -13,13 +20,23 @@ import java.sql.ResultSet
  * Remember to close the database to free up resources, if instances are created and
  * discarded besides startup and shutdown. * */
 class EphemeralDatabase(
-    name: String
+    name: String,
+    @Language("sqlite") setup: String,
 ): AutoCloseable {
     /** Creates a temporary file, so we can have multiple connections to
      * the same database. */
     private val dbFile = File.createTempFile("${name}_", ".sqlite")
     private val database = HikariDataSource().apply {
         jdbcUrl = "jdbc:sqlite:$dbFile"
+        setAutoCommit(false)
+    }
+
+    init {
+        useTransaction {
+            createStatement().use { statement ->
+                statement.executeUpdate(setup)
+            }
+        }
     }
 
     override fun close() {
@@ -27,25 +44,18 @@ class EphemeralDatabase(
         dbFile.delete()
     }
 
-    fun useConnection(block: Connection.() -> Unit) {
-        database.connection.use(block)
-    }
-
-    fun useTransaction(block: Connection.() -> Unit) {
-        database.connection.use {
-            it.autoCommit = false
+    fun <T> useTransaction(body: Connection.() -> T): T {
+        return database.connection.use { connection ->
             try {
-                block(it)
-                it.commit()
+                body(connection).also {
+                    connection.commit()
+                }
             } catch (e: Exception) {
-                it.rollback()
+                connection.rollback()
                 throw e
-            } finally {
-                it.autoCommit = true
             }
         }
     }
-
 }
 
 fun <T> Connection.usePrepareStatement(
@@ -57,8 +67,52 @@ fun <T> Connection.usePrepareStatement(
     }
 }
 
-fun <T> PreparedStatement.useExecuteQuery(block: ResultSet.() -> T): T {
-    return executeQuery().use {
-        block(it)
+fun Connection.executeUpdate(
+    @Language("sqlite") sql: String,
+    setup: Setup.() -> Unit
+): Int = usePrepareStatement(sql) {
+    Setup(this).setup()
+    executeUpdate()
+}
+
+fun <T> Connection.executeQuery(
+    @Language("sqlite") sql: String,
+    setup: Setup.() -> Unit,
+    result: ResultSet.() -> T,
+): T = usePrepareStatement(sql) {
+    Setup(this).setup()
+    executeQuery().use {
+        result(it)
     }
+}
+
+class Setup(
+    private val preparedStatement: PreparedStatement,
+) {
+    private var index: Int = 1
+
+    fun setText(text: String) = preparedStatement.setString(index++, text)
+    fun setTextOrNull(text: String?) = preparedStatement.setString(index++, text)
+
+    fun setUUID(uuid: UUID) = setText(uuid.toString())
+    fun setInstant(instant: Instant) = setText(instant.toString())
+    fun setLocalDate(localDate: LocalDate) = setText(localDate.toString())
+    fun setLocalDateOrNull(localDate: LocalDate?) = setTextOrNull(localDate?.toString())
+    fun <E: Enum<E>> setEnum(enum: E) = setText(enum.name)
+    fun <A>setJson(value: A) = setText(ephemeralDatabaseObjectMapper.writeValueAsString(value))
+}
+
+fun ResultSet.getUUID(columnLabel: String): UUID = UUID.fromString(getString(columnLabel))
+fun ResultSet.getInstant(columnLabel: String): Instant = Instant.parse(getString(columnLabel))
+fun ResultSet.getLocalDate(columnLabel: String): LocalDate = LocalDate.parse(getString(columnLabel))
+fun ResultSet.getLocalDateOrNull(columnLabel: String) = getString(columnLabel)?.let(LocalDate::parse)
+inline fun <reified E: Enum<E>> ResultSet.getEnum(columnLabel: String): E = enumValueOf(getString(columnLabel))
+inline fun <reified A>ResultSet.getJson(columnLabel: String): A =
+    ephemeralDatabaseObjectMapper.readValue(getString(columnLabel), A::class.java)
+
+/* can't be private because it's inlined in `getJson` */
+val ephemeralDatabaseObjectMapper = jacksonObjectMapper().apply {
+    registerModule(JavaTimeModule())
+    disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
 }
