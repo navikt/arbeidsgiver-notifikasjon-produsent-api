34398e51083d86fe3d262baf4878c3ea1be0264b
Ken Gullaksen <ken.gullaksen@nav.no>
2023-11-07 14:29:56 +0100

    Steg 2 Atomisk slett av sak i SkedulertHardDelete
    Her rydder vi bort alt som har med registrerte hard delete events
    
    Denne skal merges når metrikken `registered_hard_delete_events` viser 0.0 over tid

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_harddelete/SkedulertHardDeleteService.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_harddelete/SkedulertHardDeleteService.kt
@@ -2,7 +2,10 @@ package no.nav.arbeidsgiver.notifikasjon.skedulert_harddelete
 
 import no.nav.arbeidsgiver.notifikasjon.hendelse.HendelseModel
 import no.nav.arbeidsgiver.notifikasjon.hendelse.HendelseProdusent
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.*
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Health
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.NaisEnvironment
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Subsystem
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.logger
 import java.time.Instant
 import java.time.OffsetDateTime
 import java.util.*
@@ -12,7 +15,6 @@ class SkedulertHardDeleteService(
     private val hendelseProdusent: HendelseProdusent,
 ) {
     private val log = logger()
-    private val registeredHardDeleteEvents = Metrics.meterRegistry.counter("registered_hard_delete_events")
 
     suspend fun sendSkedulerteHardDeletes(tilOgMed: Instant) {
         val skalSlettes = repo.hentSkedulerteHardDeletes(tilOgMed = tilOgMed)
@@ -36,43 +38,6 @@ class SkedulertHardDeleteService(
                     merkelapp = it.merkelapp,
                 )
             )
-        }
-    }
-
-    suspend fun cascadeHardDeletes() {
-        val registrerteHardDeletes = repo.finnRegistrerteHardDeletes(100).also {
-            // when this is 0.0 over time, we can remove this method and related code
-            registeredHardDeleteEvents.increment(it.size.toDouble())
-        }
-
-
-        registrerteHardDeletes.forEach { aggregate ->
-            if (aggregate.isSak) {
-                if (aggregate.grupperingsid == null) {
-                    log.error("Sak uten grupperingsid kan ikke slettes. {}", aggregate.loggableToString())
-                    Health.subsystemAlive[Subsystem.HARDDELETE_SERVICE] = false
-                    return
-                }
-
-                repo.hentNotifikasjonerForSak(aggregate.merkelapp, aggregate.grupperingsid)
-                    .filter { it.aggregateId != aggregate.aggregateId }
-                    .forEach { notifikasjon ->
-                        hendelseProdusent.send(
-                            HendelseModel.HardDelete(
-                                hendelseId = UUID.randomUUID(),
-                                aggregateId = notifikasjon.aggregateId,
-                                virksomhetsnummer = notifikasjon.virksomhetsnummer,
-                                deletedAt = OffsetDateTime.now(),
-                                produsentId = notifikasjon.produsentid,
-                                kildeAppNavn = NaisEnvironment.clientId,
-                                grupperingsid = null,
-                                merkelapp = notifikasjon.merkelapp,
-                            )
-                        )
-                }
-            }
-
-            repo.hardDeleteCleanup(aggregate.aggregateId)
         }
     }
 }
