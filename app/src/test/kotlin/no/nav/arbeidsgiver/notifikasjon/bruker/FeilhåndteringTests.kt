91db789bd686b1c5f804dcc5bec7e0c7e6ceb285
Ken Gullaksen <ken.gullaksen@nav.no>
2023-11-08 10:50:51 +0100

    fjern bruk av mockk i FeilhåndteringTests

--- app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/FeilhåndteringTests.kt
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/FeilhåndteringTests.kt
@@ -5,37 +5,56 @@ import io.kotest.matchers.collections.beEmpty
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import io.ktor.http.*
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.mockk
 import no.nav.arbeidsgiver.notifikasjon.bruker.BrukerModel.Tilganger
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.altinn.AltinnImpl
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.altinn.SuspendingAltinnClient
+import no.nav.arbeidsgiver.notifikasjon.hendelse.HendelseModel
+import no.nav.arbeidsgiver.notifikasjon.nærmeste_leder.NarmesteLederLeesah
+import no.nav.arbeidsgiver.notifikasjon.util.AltinnStub
 import no.nav.arbeidsgiver.notifikasjon.util.getGraphqlErrors
 import no.nav.arbeidsgiver.notifikasjon.util.getTypedContent
 import no.nav.arbeidsgiver.notifikasjon.util.ktorBrukerTestServer
+import java.util.*
+
+interface BrukerRepositoryStub : BrukerRepository {
+    override suspend fun hentNotifikasjoner(fnr: String, tilganger: Tilganger): List<BrukerModel.Notifikasjon> {
+        TODO("Not yet implemented")
+    }
+    override suspend fun hentSaker(
+        fnr: String,
+        virksomhetsnummer: List<String>,
+        tilganger: Tilganger,
+        tekstsoek: String?,
+        sakstyper: List<String>?,
+        offset: Int,
+        limit: Int,
+        sortering: BrukerAPI.SakSortering,
+        oppgaveTilstand: List<BrukerModel.Oppgave.Tilstand>?
+    ) = TODO("Not yet implemented")
+    override suspend fun berikSaker(saker: List<BrukerModel.Sak>) = TODO("Not yet implemented")
+    override suspend fun oppdaterModellEtterHendelse(hendelse: HendelseModel.Hendelse, metadata: HendelseModel.HendelseMetadata) = TODO("Not yet implemented")
+    override suspend fun virksomhetsnummerForNotifikasjon(notifikasjonsid: UUID) = TODO("Not yet implemented")
+    override suspend fun oppdaterModellEtterNærmesteLederLeesah(nærmesteLederLeesah: NarmesteLederLeesah) = TODO("Not yet implemented")
+    override suspend fun hentSakstyper(fnr: String, tilganger: Tilganger) = TODO("Not yet implemented")
+    override suspend fun hentSakerForNotifikasjoner(grupperinger: List<BrukerModel.Gruppering>) : Map<String, String> = TODO("Not yet implemented")
+}
 
 class FeilhåndteringTests : DescribeSpec({
-    val brukerRepository: BrukerRepositoryImpl = mockk()
-    val suspendingAltinnClient = mockk<SuspendingAltinnClient>()
 
     val engine = ktorBrukerTestServer(
-        altinn = AltinnImpl(suspendingAltinnClient),
-        brukerRepository = brukerRepository,
+        altinn = AltinnStub { _, _ -> Tilganger.FAILURE },
+
+        brukerRepository = object : BrukerRepositoryStub {
+            override suspend fun hentNotifikasjoner(
+                fnr: String, tilganger: Tilganger
+            ) = emptyList<BrukerModel.Notifikasjon>()
+
+            override suspend fun hentSakerForNotifikasjoner(
+                grupperinger: List<BrukerModel.Gruppering>
+            ) = emptyMap<String, String>()
+        },
     )
 
     describe("graphql bruker-api feilhåndtering errors tilganger") {
         context("Feil Altinn, DigiSyfo ok") {
-            coEvery {
-                suspendingAltinnClient.hentOrganisasjoner(any(), any(), any(), any(), any())
-            } returns null
-            coEvery {
-                brukerRepository.hentNotifikasjoner(any(), any())
-            } returns listOf()
-            coEvery {
-                brukerRepository.hentSakerForNotifikasjoner(any())
-            } returns emptyMap()
-
             val response = engine.queryNotifikasjonerJson()
 
             it("status is 200 OK") {
@@ -49,7 +68,6 @@ class FeilhåndteringTests : DescribeSpec({
             it("feil Altinn") {
                 response.getTypedContent<Boolean>("notifikasjoner/feilAltinn") shouldBe true
                 response.getTypedContent<Boolean>("notifikasjoner/feilDigiSyfo") shouldBe false
-                coVerify { brukerRepository.hentNotifikasjoner(any(), Tilganger.FAILURE) }
             }
         }
     }
