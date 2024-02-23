835f9459dbee15504e2826526b56c8fe98995883
Ken Gullaksen <ken.gullaksen@nav.no>
2024-02-23 16:15:20 +0100

    keep private

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/infrastruktur/Database.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/infrastruktur/Database.kt
@@ -21,7 +21,7 @@ import java.util.*
  * are running a single command or a transaction.
  */
 class Database private constructor(
-    val config: Config,
+    private val config: Config,
     private val dataSource: NonBlockingDataSource<*>,
 ): Closeable {
     data class Config(
