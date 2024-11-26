222bdcc36b8e1b4428ae0f1fc37f79882cf62fff
Ken Gullaksen <ken.gullaksen@nav.no>
2024-11-26 12:55:23 +0100

    legg til manglende privilege

---
+++ app/src/main/resources/db/migration/bruker_model/V49__access_hard_deleted_via_cascade.sql
@@ -0,0 +1,8 @@
+grant all privileges on all tables in schema public to "notifikasjon-bruker-api";
+grant all privileges on all sequences in schema public to "notifikasjon-bruker-api";
+
+grant all privileges on all tables in schema public to "bruker-api-kafka-user";
+grant all privileges on all sequences in schema public to "bruker-api-kafka-user";
+
+grant all privileges on all tables in schema public to cloudsqliamuser;
+grant all privileges on all sequences in schema public to cloudsqliamuser;
