78f50453a1be66897c0f2fbb257d72f418a1e351
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2024-12-11 12:46:41 +0100

    Enre Label til BodyShort

--- widget/component/src/NotifikasjonWidget/NotifikasjonBjelle/NotifikasjonBjelle.tsx
+++ widget/component/src/NotifikasjonWidget/NotifikasjonBjelle/NotifikasjonBjelle.tsx
@@ -41,9 +41,9 @@ export const NotifikasjonBjelle = ({
                 : ''
             }`}
           >
-            <Label className='notifikasjon_bjelle-ikon__ulest-antall'>
+            <BodyShort className='notifikasjon_bjelle-ikon__ulest-antall'>
               {antallUleste < 10 ? antallUleste : '9+'}
-            </Label>
+            </BodyShort>
           </div>
         </div>
         <BodyShort size="small">Varsler</BodyShort>
