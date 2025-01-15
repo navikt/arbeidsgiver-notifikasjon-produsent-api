7fc59869f5e9ee2ab251bb9982579c9eea09989a
Ken Gullaksen <ken.gullaksen@nav.no>
2025-01-15 10:25:25 +0100

    ready true

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_utgått/SkedulertUtgått.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_utgått/SkedulertUtgått.kt
@@ -2,23 +2,23 @@ package no.nav.arbeidsgiver.notifikasjon.skedulert_utgått
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Database
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Health
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Subsystem
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.http.launchHttpServer
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.HendelsesstrømKafkaImpl
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.NOTIFIKASJON_TOPIC
 
 object SkedulertUtgått {
-    val databaseConfig = Database.config("skedulert_utgatt_model")
-    private val hendelsesstrøm by lazy {
-        HendelsesstrømKafkaImpl(
-            topic = NOTIFIKASJON_TOPIC,
-            groupId = "skedulert-utgatt-model-builder",
-            replayPeriodically = true,
-        )
-    }
+//    val databaseConfig = Database.config("skedulert_utgatt_model")
+//    private val hendelsesstrøm by lazy {
+//        HendelsesstrømKafkaImpl(
+//            topic = NOTIFIKASJON_TOPIC,
+//            groupId = "skedulert-utgatt-model-builder",
+//            replayPeriodically = true,
+//        )
+//    }
 
     fun main(httpPort: Int = 8080) {
         runBlocking(Dispatchers.Default) {
+            Health.subsystemReady[Subsystem.DATABASE] = true
 //            val database = openDatabaseAsync(databaseConfig)
 //            val repoAsync = async {
 //                SkedulertUtgåttRepository(database.await())
