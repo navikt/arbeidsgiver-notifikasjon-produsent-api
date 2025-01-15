e7059ef148c8531e8b94e6da5bd65fe875ad9983
Ken Gullaksen <ken.gullaksen@nav.no>
2025-01-15 10:14:28 +0100

    noop skedulert utgått midlertidig

--- app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_utgått/SkedulertUtgått.kt
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/skedulert_utgått/SkedulertUtgått.kt
@@ -1,17 +1,11 @@
 package no.nav.arbeidsgiver.notifikasjon.skedulert_utgått
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Database
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.Database.Companion.openDatabaseAsync
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.http.launchHttpServer
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.HendelsesstrømKafkaImpl
 import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.NOTIFIKASJON_TOPIC
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.lagKafkaHendelseProdusent
-import no.nav.arbeidsgiver.notifikasjon.infrastruktur.launchProcessingLoop
-import java.time.Duration
 
 object SkedulertUtgått {
     val databaseConfig = Database.config("skedulert_utgatt_model")
@@ -25,35 +19,35 @@ object SkedulertUtgått {
 
     fun main(httpPort: Int = 8080) {
         runBlocking(Dispatchers.Default) {
-            val database = openDatabaseAsync(databaseConfig)
-            val repoAsync = async {
-                SkedulertUtgåttRepository(database.await())
-            }
-            launch {
-                val repo = repoAsync.await()
-                hendelsesstrøm.forEach { hendelse, metadata ->
-                    repo.oppdaterModellEtterHendelse(hendelse)
-                }
-            }
-
-            val service = async {
-                SkedulertUtgåttService(
-                    repository = repoAsync.await(),
-                    hendelseProdusent = lagKafkaHendelseProdusent(topic = NOTIFIKASJON_TOPIC)
-                )
-            }
-            launchProcessingLoop(
-                "utgaatt-oppgaver-service",
-                pauseAfterEach = Duration.ofMinutes(1)
-            ) {
-                service.await().settOppgaverUtgåttBasertPåFrist()
-            }
-            launchProcessingLoop(
-                "avholdt-kalenderavtaler-service",
-                pauseAfterEach = Duration.ofMinutes(1)
-            ) {
-                service.await().settKalenderavtalerAvholdtBasertPåTidspunkt()
-            }
+//            val database = openDatabaseAsync(databaseConfig)
+//            val repoAsync = async {
+//                SkedulertUtgåttRepository(database.await())
+//            }
+//            launch {
+//                val repo = repoAsync.await()
+//                hendelsesstrøm.forEach { hendelse, metadata ->
+//                    repo.oppdaterModellEtterHendelse(hendelse)
+//                }
+//            }
+//
+//            val service = async {
+//                SkedulertUtgåttService(
+//                    repository = repoAsync.await(),
+//                    hendelseProdusent = lagKafkaHendelseProdusent(topic = NOTIFIKASJON_TOPIC)
+//                )
+//            }
+//            launchProcessingLoop(
+//                "utgaatt-oppgaver-service",
+//                pauseAfterEach = Duration.ofMinutes(1)
+//            ) {
+//                service.await().settOppgaverUtgåttBasertPåFrist()
+//            }
+//            launchProcessingLoop(
+//                "avholdt-kalenderavtaler-service",
+//                pauseAfterEach = Duration.ofMinutes(1)
+//            ) {
+//                service.await().settKalenderavtalerAvholdtBasertPåTidspunkt()
+//            }
 
             launchHttpServer(httpPort = httpPort)
         }
