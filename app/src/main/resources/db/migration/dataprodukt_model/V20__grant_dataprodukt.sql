1ab7e38923d36bc5122a4d4916fd133faae11852
Ken Gullaksen <ken.gullaksen@nav.no>
2025-01-24 12:56:42 +0100

    endre privileges til dataprodukt exporter

---
+++ app/src/main/resources/db/migration/dataprodukt_model/V20__grant_dataprodukt.sql
@@ -0,0 +1,4 @@
+alter default privileges in schema public
+    grant select on tables to sa_notifikasjon_dataprodukt_exporter;
+
+grant select on all tables in schema public to sa_notifikasjon_dataprodukt_exporter;
