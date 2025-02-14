ba0a17d6377828113ae409aa35622d190d11b369
Jonas Enge <jonas.maccyber.enge@nav.no>
2025-02-14 08:44:51 +0100

    Legger til kafka BigQuery sink app

---
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/kafka_bq/BigQueryHendelseService.kt
@@ -0,0 +1,31 @@
+package no.nav.arbeidsgiver.notifikasjon.kafka_bq
+
+import com.google.cloud.bigquery.Field
+import com.google.cloud.bigquery.Schema
+import com.google.cloud.bigquery.StandardSQLTypeName
+import com.google.cloud.bigquery.TableId
+import no.nav.arbeidsgiver.notifikasjon.hendelse.HendelseModel
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.BigQueryClient
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.json.laxObjectMapper
+
+class BigQueryHendelseService(
+    private val bigQueryClient: BigQueryClient,
+    private val tableName: String,
+) {
+    private val tableId: TableId by lazy {
+        bigQueryClient.getOrCreateTable(tableName, schema)
+    }
+
+    private val schema = Schema.of(
+        Field.of("hendelseId", StandardSQLTypeName.STRING),
+        Field.of("data", StandardSQLTypeName.JSON)
+    )
+
+    fun insertHendelse(hendelse: HendelseModel.Hendelse) {
+        val row = mapOf(
+            "hendelseId" to hendelse.hendelseId.toString(),
+            "data" to laxObjectMapper.writeValueAsString(hendelse)
+        )
+        bigQueryClient.insert(tableId, hendelse.hendelseId.toString(), row)
+    }
+}
---
+++ app/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/kafka_bq/KafkaBQ.kt
@@ -0,0 +1,42 @@
+package no.nav.arbeidsgiver.notifikasjon.kafka_bq
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.BigQueryClientImpl
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.http.launchHttpServer
+import no.nav.arbeidsgiver.notifikasjon.infrastruktur.kafka.HendelsesstrømKafkaImpl
+
+object KafkaBQ {
+    private val hendelsesstrøm by lazy {
+        HendelsesstrømKafkaImpl(groupId = "kafka-bq-v1")
+    }
+    private val projectId = System.getenv("GCP_TEAM_PROJECT_ID")
+        ?: error("Missing required environment variable: GCP_TEAM_PROJECT_ID")
+    private val datasetId = System.getenv("BIGQUERY_DATASET_ID")
+        ?: error("Missing required environment variable: BIGQUERY_DATASET_ID")
+    private val tableName = System.getenv("BIGQUERY_TABLE_NAME")
+        ?: error("Missing required environment variable: BIGQUERY_TABLE_NAME")
+
+    private val bigQueryHendelseService by lazy {
+        BigQueryHendelseService(
+            tableName = tableName,
+            bigQueryClient = BigQueryClientImpl(
+                projectId = projectId,
+                datasetId = datasetId
+            ),
+        )
+    }
+
+    fun main(httpPort: Int = 8080) {
+        runBlocking(Dispatchers.Default) {
+            launch {
+                hendelsesstrøm.forEach { hendelse ->
+                    bigQueryHendelseService.insertHendelse(hendelse)
+                }
+            }
+
+            launchHttpServer(httpPort = httpPort)
+        }
+    }
+}
