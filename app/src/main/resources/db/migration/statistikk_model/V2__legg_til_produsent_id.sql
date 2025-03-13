868dc745b65e98147627d0eb331d90264c562005
Jonas Enge <jonas.maccyber.enge@nav.no>
2025-03-13 14:23:18 +0100

    Sletter statistikk app og alle referanser til den

--- app/src/main/resources/db/migration/statistikk_model/V2__legg_til_produsent_id.sql
+++
@@ -1,3 +0,0 @@
-alter table notifikasjon_statistikk add column produsent_id text;
-
-create index produsent_id_idx on notifikasjon_statistikk (produsent_id);
--- local-db-init.sql
+++ local-db-init.sql
@@ -12,9 +12,6 @@ GRANT ALL PRIVILEGES ON DATABASE "produsent-model" TO postgres;
 CREATE DATABASE "kafka-reaper-model";
 GRANT ALL PRIVILEGES ON DATABASE "kafka-reaper-model" TO postgres;
 
-CREATE DATABASE "statistikk-model";
-GRANT ALL PRIVILEGES ON DATABASE "statistikk-model" TO postgres;
-
 CREATE DATABASE "ekstern-varsling-model";
 GRANT ALL PRIVILEGES ON DATABASE "ekstern-varsling-model" TO postgres;
 
