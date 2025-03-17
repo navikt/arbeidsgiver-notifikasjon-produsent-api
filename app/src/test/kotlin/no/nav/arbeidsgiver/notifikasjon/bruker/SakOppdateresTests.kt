0f14c30b8b85d88578da4344869025d5590a13bf
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-03-17 11:19:06 +0100

    Fiks testene

--- app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/SakOppdateresTests.kt
+++ app/src/test/kotlin/no/nav/arbeidsgiver/notifikasjon/bruker/SakOppdateresTests.kt
@@ -19,7 +19,6 @@ class SakOppdateresTests : DescribeSpec({
                 select sist_endret_tidspunkt
                 from sak
                 where id = ?
-                
             """.trimIndent(),
             {
                 uuid(sakId)
@@ -34,64 +33,77 @@ class SakOppdateresTests : DescribeSpec({
     describe("Sak oppdateres") {
         it("på OppgaveOpprettet") {
             val sakOpprettet = brukerRepository.sakOpprettet(
-                grupperingsid = "grupperingsid",
                 merkelapp = "merkelapp",
                 oppgittTidspunkt = OffsetDateTime.now().minusDays(7),
             )
-            val oppgave = brukerRepository.oppgaveOpprettet(sak = sakOpprettet, opprettetTidspunkt = sakOpprettet.oppgittTidspunkt!!.plusDays(1))
+            val oppgave = brukerRepository.oppgaveOpprettet(
+                sak = sakOpprettet,
+                opprettetTidspunkt = sakOpprettet.oppgittTidspunkt!!.plusDays(1)
+            )
             val sistEndretTidspunkt = hentSakSisteEndretTidspunktById(sakOpprettet.sakId)
             sistEndretTidspunkt shouldBe oppgave.opprettetTidspunkt.toInstant()
         }
 
         it("på BeskjedOpprettet") {
             val sakOpprettet = brukerRepository.sakOpprettet(
-                grupperingsid = "grupperingsid",
                 merkelapp = "merkelapp",
                 oppgittTidspunkt = OffsetDateTime.now().minusDays(7),
             )
-            val beskjed = brukerRepository.beskjedOpprettet(sak = sakOpprettet, opprettetTidspunkt = sakOpprettet.oppgittTidspunkt!!.plusDays(1))
+            val beskjed = brukerRepository.beskjedOpprettet(
+                sak = sakOpprettet,
+                opprettetTidspunkt = sakOpprettet.oppgittTidspunkt!!.plusDays(1)
+            )
             val sistEndretTidspunkt = hentSakSisteEndretTidspunktById(sakOpprettet.sakId)
             sistEndretTidspunkt shouldBe beskjed.opprettetTidspunkt.toInstant()
         }
 
-        it("oppdateres ikke på OppgaveOpprettet me opprettetTidspunkt før sist_endret_tidspunkt"){
+        it("oppdateres ikke på OppgaveOpprettet me opprettetTidspunkt før sist_endret_tidspunkt") {
             val sakOpprettet = brukerRepository.sakOpprettet(
-                grupperingsid = "grupperingsid",
                 merkelapp = "merkelapp",
                 oppgittTidspunkt = OffsetDateTime.now().minusDays(7),
             )
-            val oppgave = brukerRepository.oppgaveOpprettet(sak = sakOpprettet, opprettetTidspunkt = sakOpprettet.oppgittTidspunkt!!.minusDays(1))
+            val oppgave = brukerRepository.oppgaveOpprettet(
+                sak = sakOpprettet,
+                opprettetTidspunkt = sakOpprettet.oppgittTidspunkt!!.minusDays(1)
+            )
             val sistEndretTidspunkt = hentSakSisteEndretTidspunktById(sakOpprettet.sakId)
             sistEndretTidspunkt shouldBe sakOpprettet.oppgittTidspunkt.toInstant()
         }
 
         it("på KalenderavtaleOpprettet") {
             val sakOpprettet = brukerRepository.sakOpprettet(
-                grupperingsid = "grupperingsid",
                 merkelapp = "merkelapp",
                 oppgittTidspunkt = OffsetDateTime.now().minusDays(7),
             )
-            val kalenderavtale = brukerRepository.kalenderavtaleOpprettet(sak = sakOpprettet, opprettetTidspunkt = sakOpprettet.oppgittTidspunkt!!.plusDays(1))
+            val kalenderavtale = brukerRepository.kalenderavtaleOpprettet(
+                sak = sakOpprettet,
+                opprettetTidspunkt = sakOpprettet.oppgittTidspunkt!!.plusDays(1)
+            )
             val sistEndretTidspunkt = hentSakSisteEndretTidspunktById(sakOpprettet.sakId)
             sistEndretTidspunkt shouldBe kalenderavtale.opprettetTidspunkt.toInstant()
         }
         it("på NyStatusSak") {
             val sakOpprettet = brukerRepository.sakOpprettet(
-                grupperingsid = "grupperingsid",
                 merkelapp = "merkelapp",
                 oppgittTidspunkt = OffsetDateTime.now().minusDays(7),
             )
-            val statusSak = brukerRepository.nyStatusSak(sak = sakOpprettet, idempotensKey = "nei", oppgittTidspunkt = sakOpprettet.oppgittTidspunkt!!.plusDays(1))
+            val statusSak = brukerRepository.nyStatusSak(
+                sak = sakOpprettet,
+                idempotensKey = "nei",
+                oppgittTidspunkt = sakOpprettet.oppgittTidspunkt!!.plusDays(1)
+            )
             val sistEndretTidspunkt = hentSakSisteEndretTidspunktById(sakOpprettet.sakId)
             sistEndretTidspunkt shouldBe statusSak.opprettetTidspunkt.toInstant()
         }
         it("på PåminnelseOpprettet") {
             val sakOpprettet = brukerRepository.sakOpprettet(
-                grupperingsid = "grupperingsid",
                 merkelapp = "merkelapp",
                 oppgittTidspunkt = OffsetDateTime.now().minusDays(7),
             )
-            val oppgave = brukerRepository.oppgaveOpprettet(sak = sakOpprettet, opprettetTidspunkt = sakOpprettet.oppgittTidspunkt!!.plusDays(1))
+            val oppgave = brukerRepository.oppgaveOpprettet(
+                sak = sakOpprettet,
+                opprettetTidspunkt = sakOpprettet.oppgittTidspunkt!!.plusDays(1)
+            )
             val påminnelsesTidspunkt = oppgave.opprettetTidspunkt.plusDays(7)
             val påminnelse = brukerRepository.påminnelseOpprettet(
                 oppgave,
