f7a7aa31c9bdb8d998f240fecd72e26d07820096
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-04-07 13:15:51 +0200

    Start å fikse ny bjelle

--- widget/component/src/NotifikasjonWidget/NotifikasjonBjelle/NotifikasjonBjelle.tsx
+++ widget/component/src/NotifikasjonWidget/NotifikasjonBjelle/NotifikasjonBjelle.tsx
@@ -1,59 +1,59 @@
-import React, { Ref } from 'react'
-import './NotifikasjonBjelle.css'
-import {BodyShort} from "@navikt/ds-react";
-import {Bell} from "@navikt/ds-icons";
+import React, { Ref } from 'react';
+import './NotifikasjonBjelle.css';
+import { BodyShort } from '@navikt/ds-react';
+import { BellFillIcon } from '@navikt/aksel-icons';
 
 interface Props {
-  antallUleste?: number
-  erApen: boolean
-  onClick?: () => void
-  focusableRef: Ref<HTMLButtonElement>
+  antallUleste?: number;
+  erApen: boolean;
+  onClick?: () => void;
+  focusableRef: Ref<HTMLButtonElement>;
 }
 
 export const NotifikasjonBjelle = ({
-  antallUleste = 0,
-  erApen,
-  onClick,
-  focusableRef
-}: Props) => {
+                                     antallUleste = 0,
+                                     erApen,
+                                     onClick,
+                                     focusableRef,
+                                   }: Props) => {
+
   return (
-    <div className='notifikasjon_bjelle'>
+    <div className={`notifikasjon_bjelle ${antallUleste > 0 ? 'notifikasjon_bjelle_uleste' : ''}`}>
       <button
         ref={focusableRef}
         onClick={onClick}
-        className={`notifikasjon_bjelle-knapp notifikasjon_bjelle-knapp${
-          erApen ? '--open' : ''
-        }`}
+        className={`notifikasjon_bjelle-knapp`}
         aria-label={`Dine varsler, ${antallUleste} nye.`}
-        aria-owns='notifikasjon_panel'
-        aria-haspopup='dialog'
+        aria-owns="notifikasjon_panel"
+        aria-haspopup="dialog"
         aria-pressed={erApen}
         aria-live="polite"
         aria-atomic="true"
       >
-        <div className={`notifikasjon_bjelle-ikon ${antallUleste > 0 ? "notifikasjon_bjelle-ikon-svinger":""}`}>
-          <Bell width="32px" height="32px" aria-hidden="true" />
-
-          <div
-            className={`notifikasjon_bjelle-ikon__ulest-sirkel ${
-              antallUleste === 0
-                ? 'notifikasjon_bjelle-ikon__ulest-sirkel--hide'
-                : ''
-            }`}
-          >
-            <BodyShort className='notifikasjon_bjelle-ikon__ulest-antall'>
-              {antallUleste < 10 ? antallUleste : '9+'}
-            </BodyShort>
-          </div>
+        <div className={`notifikasjon_bjelle-ikon`}>
+          {antallUleste === 0 ?
+            (
+              <BellFillIcon width="28px" height="28px" aria-hidden="true" />
+            ) : (
+              <>
+                  <BellFillIcon width="28px" height="28px" aria-hidden="true" />
+                <div
+                  className={`notifikasjon_bjelle-ikon__ulest-sirkel ${
+                    antallUleste === 0
+                      ? 'notifikasjon_bjelle-ikon__ulest-sirkel--hide'
+                      : ''
+                  }`}
+                >
+                  <BodyShort className="notifikasjon_bjelle-ikon__ulest-antall">
+                    {antallUleste < 10 ? antallUleste : '9+'}
+                  </BodyShort>
+                </div>
+              </>
+            )
+          }
         </div>
         <BodyShort size="small">Varsler</BodyShort>
-
       </button>
-      <div
-        className={`notifikasjon_bjelle-understrek ${
-          erApen ? '' : 'notifikasjon_bjelle-understrek--closed'
-        }`}
-      />
     </div>
-  )
+  );
 }
